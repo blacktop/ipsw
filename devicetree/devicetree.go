b8d31c49caa5fa6b866b6e2d66f146bad45bc1b5
blacktop <blacktop@users.noreply.github.com>
2019-05-17 00:03:43 -0400

    updates

--- devicetree/devicetree.go
+++ devicetree/devicetree.go
@@ -3,10 +3,12 @@ package devicetree
 import (
 	"bytes"
 	"encoding/asn1"
+	"encoding/base64"
 	"encoding/binary"
 	"fmt"
 	"math"
 	"os"
+	"unicode"
 
 	"github.com/apex/log"
 )
@@ -29,6 +31,45 @@ type Node struct {
 type NodeProperty struct {
 	Name   [32]byte // NUL terminated property name (max length 32)
 	Length uint32   // Length (bytes) of folloing prop value
+}
+
+func isASCIIPrintable(s string) bool {
+	for _, r := range s {
+		if r > unicode.MaxASCII || !unicode.IsPrint(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func parseValue(value []byte) interface{} {
+	// remove trailing NULLs
+	value = bytes.TrimRight(value[:], "\x00")
+	// value is a string
+	if isASCIIPrintable(string(value)) {
+		return string(value)
+	}
+	parts := bytes.Split(value, []byte("\x00"))
+	// value is a string array
+	if len(parts) > 1 {
+		var values []string
+		for _, part := range parts {
+			if len(string(part)) > 0 {
+				if isASCIIPrintable(string(part)) {
+					values = append(values, string(part))
+				} // else {
+				// 	values = append(values, base64.StdEncoding.EncodeToString(value))
+				// }
+			}
+		}
+		return values
+	}
+	if len(value) < 3 {
+		i, _ := binary.Uvarint(value)
+		return i
+	}
+	// value is data
+	return base64.StdEncoding.EncodeToString(value)
 }
 
 // Parse parses a DeviceTree img4 file
@@ -61,8 +102,9 @@ func Parse(path string) error {
 
 	var node Node
 	var prop NodeProperty
-	var vlen uint32
+
 	fmt.Println("# DeviceTree")
+
 	for buffer.Len() > 0 {
 		if err = binary.Read(buffer, binary.LittleEndian, &node); err != nil {
 			return err
@@ -72,26 +114,19 @@ func Parse(path string) error {
 			if err = binary.Read(buffer, binary.LittleEndian, &prop); err != nil {
 				return err
 			}
-			vlen = prop.Length & (math.MaxUint32 >> 1)
-			if (vlen % 4) != 0 {
-				vlen = vlen + (4 - (vlen % 4))
+			prop.Length &= math.MaxInt32
+			if (prop.Length % 4) != 0 {
+				prop.Length = prop.Length + (4 - (prop.Length % 4))
 			}
-			dat = make([]byte, vlen)
+			dat = make([]byte, prop.Length)
 			if err = binary.Read(buffer, binary.LittleEndian, &dat); err != nil {
 				return err
 			}
+
 			name := string(bytes.TrimRight(prop.Name[:], "\x00"))
-			value := string(bytes.TrimRight(dat[:], "\x00"))
-			// values := bytes.Split(bytes.TrimSuffix(dat[:], []byte("\x00")), []byte("\x00"))
-			// if len(values) > 1 {
-			// 	fmt.Printf("%s =>\n", name)
-			// 	for _, v := range values {
-			// 		fmt.Printf("\t - %s\n", string(v))
-			// 	}
-			// } else {
-			// 	value := string(values[0])
-			fmt.Printf("\t\t- %s => %s\n", name, value)
-			// }
+			value := parseValue(dat)
+
+			fmt.Printf("\t\t- %s => %v\n", name, value)
 		}
 	}
 	return nil
