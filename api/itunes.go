827fa62a27cf191dced667c9cdb1fb9b4786052e
blacktop <blacktop@users.noreply.github.com>
2019-05-19 20:05:51 -0400

    fix itunes downloading

--- api/itunes.go
+++ api/itunes.go
@@ -19,6 +19,15 @@ type Identifier string
 // BuildNumber object
 type BuildNumber string
 
+// Build object
+type Build struct {
+	Identifier     string `json:"identifier,omitempty"`
+	BuildVersion   string `json:"build_version,omitempty"`
+	FirmwareURL    string `json:"firmware_url,omitempty"`
+	FirmwareSHA1   string `json:"firmware_sha1,omitempty"`
+	ProductVersion string `json:"product_version,omitempty"`
+}
+
 // IndividualBuild object
 type IndividualBuild struct {
 	BuildVersion     BuildNumber
@@ -46,6 +55,70 @@ type ITunesVersionMaster struct {
 	MobileDeviceSoftwareVersionsByVersion map[string]*VersionWrapper
 }
 
+// UniqueBuilds returns a slice with Builds with unique FirmwareURLs
+func UniqueBuilds(b []Build) []Build {
+	unique := make(map[string]bool, len(b))
+	bs := make([]Build, len(unique))
+	for _, elem := range b {
+		if len(elem.FirmwareURL) != 0 {
+			if !unique[elem.FirmwareURL] {
+				bs = append(bs, elem)
+				unique[elem.FirmwareURL] = true
+			}
+		}
+	}
+
+	return bs
+}
+
+// GetBuilds gets all the IndividualBuilds
+func (vm *ITunesVersionMaster) GetBuilds() []Build {
+	var b []Build
+	for _, deviceSoftwareVersions := range vm.MobileDeviceSoftwareVersionsByVersion {
+		for identifier, builds := range deviceSoftwareVersions.MobileDeviceSoftwareVersions {
+			for _, build := range builds {
+				if build.Restore != nil {
+					if strings.Contains(build.Restore.FirmwareURL, "protected://") {
+						continue
+					}
+					build := Build{
+						Identifier:     string(identifier),
+						BuildVersion:   string(build.Restore.BuildVersion),
+						FirmwareURL:    build.Restore.FirmwareURL,
+						FirmwareSHA1:   build.Restore.FirmwareSHA1,
+						ProductVersion: build.Restore.ProductVersion,
+					}
+					b = append(b, build)
+				}
+			}
+		}
+	}
+
+	return b
+}
+
+// GetBuildsForVersion gets all the IndividualBuilds that match supplied version
+func (vm *ITunesVersionMaster) GetBuildsForVersion(version string) []Build {
+	var builds []Build
+	for _, build := range vm.GetBuilds() {
+		if build.ProductVersion == version {
+			builds = append(builds, build)
+		}
+	}
+	return UniqueBuilds(builds)
+}
+
+// GetBuildsForBuildID gets all the IndividualBuilds that match supplied build ID
+func (vm *ITunesVersionMaster) GetBuildsForBuildID(buildID string) []Build {
+	var builds []Build
+	for _, build := range vm.GetBuilds() {
+		if build.BuildVersion == buildID {
+			builds = append(builds, build)
+		}
+	}
+	return UniqueBuilds(builds)
+}
+
 // GetSoftwareURLFor gets the latest ipsw download URL for a device. i.e. "iPhone11,2"
 func (vm *ITunesVersionMaster) GetSoftwareURLFor(device string) (string, error) {
 
@@ -67,8 +140,8 @@ func (vm *ITunesVersionMaster) GetSoftwareURLFor(device string) (string, error) 
 	return "", errors.Errorf("unable to find url for device: %s", device)
 }
 
-// GetSoftwareURLsForVersion gets all the ipsw URLs for an iOS version
-func (vm *ITunesVersionMaster) GetSoftwareURLsForVersion(version string) ([]string, error) {
+// GetSoftwareURLs gets all the ipsw URLs
+func (vm *ITunesVersionMaster) GetSoftwareURLs() ([]string, error) {
 	urls := []string{}
 	for _, deviceSoftwareVersions := range vm.MobileDeviceSoftwareVersionsByVersion {
 		for _, builds := range deviceSoftwareVersions.MobileDeviceSoftwareVersions {
@@ -78,9 +151,9 @@ func (vm *ITunesVersionMaster) GetSoftwareURLsForVersion(version string) ([]stri
 					if strings.Contains(build.Restore.FirmwareURL, "protected://") {
 						continue
 					}
-					if build.Restore.ProductVersion == version {
-						urls = append(urls, build.Restore.FirmwareURL)
-					}
+					// if build.Restore.ProductVersion == version {
+					urls = append(urls, build.Restore.FirmwareURL)
+					// }
 				}
 			}
 		}
@@ -88,22 +161,23 @@ func (vm *ITunesVersionMaster) GetSoftwareURLsForVersion(version string) ([]stri
 	return utils.Unique(urls), nil
 }
 
+// GetSoftwareURLsForVersion gets all the ipsw URLs for an iOS version
+func (vm *ITunesVersionMaster) GetSoftwareURLsForVersion(version string) ([]string, error) {
+	var urls []string
+	for _, build := range vm.GetBuilds() {
+		if build.ProductVersion == version {
+			urls = append(urls, build.FirmwareURL)
+		}
+	}
+	return utils.Unique(urls), nil
+}
+
 // GetSoftwareURLsForBuildID gets all the ipsw URLs for an iOS Build ID
 func (vm *ITunesVersionMaster) GetSoftwareURLsForBuildID(buildID string) ([]string, error) {
-	urls := []string{}
-	for _, deviceSoftwareVersions := range vm.MobileDeviceSoftwareVersionsByVersion {
-		for _, builds := range deviceSoftwareVersions.MobileDeviceSoftwareVersions {
-			for _, build := range builds {
-				if build.Restore != nil {
-					// don't return protected ones if we can avoid it
-					if strings.Contains(build.Restore.FirmwareURL, "protected://") {
-						continue
-					}
-					if string(build.Restore.BuildVersion) == buildID {
-						urls = append(urls, build.Restore.FirmwareURL)
-					}
-				}
-			}
+	var urls []string
+	for _, build := range vm.GetBuilds() {
+		if build.BuildVersion == buildID {
+			urls = append(urls, build.FirmwareURL)
 		}
 	}
 	return utils.Unique(urls), nil
