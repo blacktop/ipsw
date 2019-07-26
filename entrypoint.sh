623e630f3447bb4a26f16e9f3897260b23d25268
blacktop <blacktop@users.noreply.github.com>
2019-07-26 08:43:21 -0400

    umount

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ unzip -j /data/*.ipsw '*.dmg' -d /tmp
 
 DMG=$(find /tmp -type f -printf '%s %p\n' | sort -nr | head -n1 | awk '{print $2;}')
 
+echo " > Mounting $DMG at /app..."
 apfs-fuse $DMG /app
 
+echo " > Copying dyld_shared_cache..."
 cp /app/root/System/Library/Caches/com.apple.dyld/dyld_shared_cache_* /data
+
+echo " > Unmounting /app..."
+umount /app
