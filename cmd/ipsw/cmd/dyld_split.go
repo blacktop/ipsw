3e2bbc818f32d4941f43bbdd233d04dd2cce0338
blacktop <blacktop@users.noreply.github.com>
2019-09-01 12:08:49 -0400

    tweeks

--- cmd/ipsw/cmd/dyld_split.go
+++ cmd/ipsw/cmd/dyld_split.go
@@ -24,18 +24,68 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/apex/log"
 	"github.com/blacktop/ipsw/dyld"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
+
+var deviceOsFlag string
 
 func init() {
 	dyldCmd.AddCommand(splitCmd)
+
+	splitCmd.Flags().BoolP("iphone", "i", true, "Extract from iPhoneOS cache")
+	splitCmd.Flags().BoolP("appletv", "a", false, "Extract from AppleTVOS cache")
+	splitCmd.Flags().BoolP("watch", "w", false, "Extract from WatchOS cache")
+}
+
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}
+
+func checkRequiredFlags(flags *pflag.FlagSet) error {
+	requiredError := false
+	flagSetCount := 0
+
+	flags.VisitAll(func(flag *pflag.Flag) {
+		if contains([]string{"iphone", "appletv", "watch"}, flag.Name) {
+			if strings.EqualFold(flag.Value.String(), "true") {
+				flagSetCount++
+				switch flag.Name {
+				case "iphone":
+					deviceOsFlag = "iPhoneOS"
+				case "appletv":
+					deviceOsFlag = "AppleTVOS"
+				case "watch":
+					deviceOsFlag = "WatchOS"
+				}
+			}
+
+			if flagSetCount > 1 {
+				requiredError = true
+			}
+		}
+	})
+
+	if requiredError {
+		return errors.New("Flags are mutually exclusive: please only set one of -i|-a|-w")
+	}
+
+	return nil
 }
 
 // splitCmd represents the split command
@@ -43,6 +93,9 @@ var splitCmd = &cobra.Command{
 	Use:   "split [path to dyld_shared_cache]",
 	Short: "Extracts all the dyld_shared_cache libraries",
 	Args:  cobra.MinimumNArgs(1),
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		return checkRequiredFlags(cmd.Flags())
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 
 		if Verbose {
@@ -60,6 +113,6 @@ var splitCmd = &cobra.Command{
 		}
 
 		log.Info("Splitting dyld_shared_cache")
-		return dyld.Split(dscPath, filepath.Dir(dscPath))
+		return dyld.Split(dscPath, filepath.Dir(dscPath), deviceOsFlag)
 	},
 }
