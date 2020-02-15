73f8cda20be3aaa1af021cf20a27524e6c613334
blacktop <blacktop@users.noreply.github.com>
2020-02-15 09:25:27 -0500

    chore: move asm

--- pkg/lzfse/lzfse_decode_amd64.go
+++ pkg/lzfse/lzfse_decode_amd64.go
@@ -8,23 +8,25 @@ import (
 )
 
 //go:noescape
-func _lzfse_decode_scratch_size() uint32
+func __lzfse_decode_scratch_size(result uint64)
 
 //go:noescape
-func _lzfse_decode_buffer(dstBuffer unsafe.Pointer, dstSize uint64, srcBuffer unsafe.Pointer, srcSize uint64, scratchBuffer unsafe.Pointer)
+func __lzfse_decode_buffer(dstBuffer unsafe.Pointer, dstSize uint64, srcBuffer unsafe.Pointer, srcSize uint64, scratchBuffer unsafe.Pointer)
 
 //go:noescape
-func _lzfse_decode_buffer_with_scratch(dstBuffer unsafe.Pointer, dstSize uint64, srcBuffer unsafe.Pointer, srcSize uint64, scratchBuffer unsafe.Pointer)
+func __lzfse_decode_buffer_with_scratch(dstBuffer unsafe.Pointer, dstSize uint64, srcBuffer unsafe.Pointer, srcSize uint64, scratchBuffer unsafe.Pointer)
 
-func lzfse_decode_scratch_size() uint32 {
-	return _lzfse_decode_scratch_size()
+func lzfse_decode_scratch_size() uint64 {
+	var result uint64
+	__lzfse_decode_scratch_size(result)
+	return result
 }
 
-func lzfse_decode_buffer(srcBuffer []byte) []byte {
+func DecodeBuffer(srcBuffer []byte) []byte {
 
 	dstBuffer := make([]byte, 4*len(srcBuffer))
 	scratchBuffer := make([]byte, lzfse_decode_scratch_size())
-	_lzfse_decode_buffer(unsafe.Pointer(&dstBuffer), uint64(len(dstBuffer)), unsafe.Pointer(&dstBuffer), uint64(len(dstBuffer)), unsafe.Pointer(&scratchBuffer))
+	__lzfse_decode_buffer(unsafe.Pointer(&dstBuffer), uint64(len(dstBuffer)), unsafe.Pointer(&dstBuffer), uint64(len(dstBuffer)), unsafe.Pointer(&scratchBuffer))
 
 	return dstBuffer
 }
