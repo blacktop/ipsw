d6a4b5d4b2fd3712d398f56699858f748509b1e3
blacktop <blacktop@users.noreply.github.com>
2020-02-23 21:17:39 -0500

    feat: add 🆕download ota cmd

--- pkg/ota/ota.go
+++ pkg/ota/ota.go
@@ -1,3 +1,5 @@
+// +build !windows,cgo
+
 package ota
 
 import (
@@ -6,15 +8,20 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"strings"
+	"text/tabwriter"
 
 	"github.com/apex/log"
-	"github.com/blacktop/xz"
+	"github.com/dustin/go-humanize"
+
+	// "github.com/blacktop/xz"
+	"github.com/danielrh/go-xz"
 	"github.com/pkg/errors"
-	// "github.com/danielrh/go-xz"
 )
 
 const (
@@ -53,8 +60,14 @@ type entry struct {
 	// Followed by file contents
 }
 
+func sortFileBySize(files []*zip.File) {
+	sort.Slice(files, func(i, j int) bool {
+		return files[i].UncompressedSize64 > files[j].UncompressedSize64
+	})
+}
+
 // Extract extracts and decompresses OTA payload files
-func Extract(otaZIP string) error {
+func Extract(otaZIP, extractPattern string, listFiles bool) error {
 
 	zr, err := zip.OpenReader(otaZIP)
 	if err != nil {
@@ -64,14 +77,14 @@ func Extract(otaZIP string) error {
 
 	var validPayload = regexp.MustCompile(`payload.0\d+$`)
 
+	sortFileBySize(zr.File)
+
 	for _, f := range zr.File {
 		if validPayload.MatchString(f.Name) {
-			fmt.Println(f.Name)
-			if strings.Contains(f.Name, "payload.004") {
-				err = ParseOTA(f)
-				if err != nil {
-					log.Error(err.Error())
-				}
+			log.WithFields(log.Fields{"filename": f.Name, "size": humanize.Bytes(f.UncompressedSize64)}).Debug("Processing OTA file")
+			err = ParseOTA(f, listFiles, extractPattern)
+			if err != nil {
+				log.Error(err.Error())
 			}
 		}
 	}
@@ -79,8 +92,29 @@ func Extract(otaZIP string) error {
 	return nil
 }
 
+func RemoteParseOTA(zr *zip.Reader) error {
+
+	var validPayload = regexp.MustCompile(`payload.0\d+$`)
+
+	sortFileBySize(zr.File)
+
+	for _, f := range zr.File {
+		if validPayload.MatchString(f.Name) {
+			log.WithFields(log.Fields{"filename": f.Name, "size": humanize.Bytes(f.UncompressedSize64)}).Debug("Processing OTA file")
+			err := ParseOTA(f, false, "dyld_shared_cache_")
+			if err != nil {
+				log.Error(err.Error())
+			}
+			return nil
+		}
+	}
+
+	return fmt.Errorf("dyld_shared_cache not found")
+}
+
 // ParseOTA parses a ota payload file inside the zip
-func ParseOTA(payload *zip.File) error {
+func ParseOTA(payload *zip.File, listFiles bool, extractPattern string) error {
+	var w *tabwriter.Writer
 
 	pData := make([]byte, payload.UncompressedSize64)
 
@@ -103,11 +137,13 @@ func ParseOTA(payload *zip.File) error {
 		return errors.New("src not a pbzx stream")
 	}
 
-	f, err := os.Create(filepath.Base(payload.Name) + ".xz")
-	if err != nil {
-		return errors.Wrapf(err, "failed to create file: %s", filepath.Base(payload.Name)+".xz")
-	}
-	defer f.Close()
+	// f, err := os.Create(filepath.Base(payload.Name) + ".xz")
+	// if err != nil {
+	// 	return errors.Wrapf(err, "failed to create file: %s", filepath.Base(payload.Name)+".xz")
+	// }
+	// defer f.Close()
+
+	xzBuf := new(bytes.Buffer)
 
 	for {
 		var xzTag xzHeader
@@ -115,40 +151,103 @@ func ParseOTA(payload *zip.File) error {
 			return err
 		}
 
-		xzBuf := make([]byte, xzTag.Size)
-		if err := binary.Read(pr, binary.BigEndian, &xzBuf); err != nil {
+		xzChunkBuf := make([]byte, xzTag.Size)
+		if err := binary.Read(pr, binary.BigEndian, &xzChunkBuf); err != nil {
 			return err
 		}
-		// xr := xz.NewDecompressionReader(bytes.NewReader(xzBuf))
-		xr, err := xz.NewReader(bytes.NewReader(xzBuf))
-		if err != nil {
-			return err
-		}
+		xr := xz.NewDecompressionReader(bytes.NewReader(xzChunkBuf))
+		// xr, err := xz.NewReader(bytes.NewReader(xzBuf))
+		// if err != nil {
+		// 	return err
+		// }
 		dstBuf := make([]byte, pbzx.UncompressedSize)
+		xzBuf.Grow(int(pbzx.UncompressedSize))
+
 		_, err = xr.Read(dstBuf)
 		if err != nil {
 			return err
 		}
-		// write chunk to file
-		f.Write(dstBuf)
 
-		rr := bytes.NewReader(dstBuf)
-		var e entry
-		if err := binary.Read(rr, binary.BigEndian, &e); err != nil {
-			return err
-		}
-		fileName := make([]byte, e.NameLen)
-		if err := binary.Read(rr, binary.BigEndian, &fileName); err != nil {
-			return err
-		}
-		fmt.Println(string(fileName))
+		// xzBuf.ReadFrom(xr)
+		xzBuf.Write(dstBuf)
 
-		if (xzTag.Flags & hasMoreChunks) != 0 {
+		if (xzTag.Flags & hasMoreChunks) == 0 {
+			// fmt.Printf("0x%x\n", xzTag.Flags)
+			xr.Close()
 			break
 		}
 	}
 
-	f.Sync()
+	rr := bytes.NewReader(xzBuf.Bytes())
+
+	if listFiles {
+		w = tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.DiscardEmptyColumns)
+	}
+
+	for {
+		var e entry
+		if err := binary.Read(rr, binary.BigEndian, &e); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		if e.Usually_0x210Or_0x110 != 0x10010000 && e.Usually_0x210Or_0x110 != 0x10020000 && e.Usually_0x210Or_0x110 != 0x10030000 {
+			// 0x10030000 seem to be framworks and other important platform binaries
+			// if e.Usually_0x210Or_0x110 != 0 {
+			// 	log.Warnf("found unknown entry flag: 0x%x", e.Usually_0x210Or_0x110)
+			// }
+			break
+		}
+
+		fileName := make([]byte, e.NameLen)
+		if err := binary.Read(rr, binary.BigEndian, &fileName); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		// if e.Usually_0x20 != 0x20 {
+		// 	fmt.Printf("%s: %#v\n", fileName, e)
+		// }
+
+		// if e.Usually_0x00_00 != 0 {
+		// 	fmt.Printf("%s: %#v\n", fileName, e)
+		// }
+
+		if e.Usually_0x210Or_0x110 == 0x10030000 {
+			fmt.Printf("%s (%s): %#v\n", fileName, os.FileMode(e.Perms), e)
+		}
+
+		if listFiles {
+			fmt.Fprintf(w, "%s\tuid=%d\tgid=%d\t%s\t%s\n", os.FileMode(e.Perms), e.Uid, e.Gid, humanize.Bytes(uint64(e.FileSize)), fileName)
+		}
+
+		if len(extractPattern) > 0 {
+			if strings.Contains(strings.ToLower(string(fileName)), strings.ToLower(extractPattern)) {
+				fileBytes := make([]byte, e.FileSize)
+				if err := binary.Read(rr, binary.BigEndian, &fileBytes); err != nil {
+					if err == io.EOF {
+						break
+					}
+					return err
+				}
+				log.Infof("Extracting %s uid=%d, gid=%d, %s, %s\n", os.FileMode(e.Perms), e.Uid, e.Gid, humanize.Bytes(uint64(e.FileSize)), fileName)
+				err = ioutil.WriteFile(filepath.Base(string(fileName)), fileBytes, 0644)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			rr.Seek(int64(e.FileSize), io.SeekCurrent)
+		}
+	}
+
+	if listFiles {
+		w.Flush()
+	}
 
 	return nil
 }
