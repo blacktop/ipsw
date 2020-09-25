c5c786c949327bba8386a34a100c0916845fb228
blacktop <blacktop@users.noreply.github.com>
2020-09-25 15:06:57 -0400

    Update devicetree_windows.go

--- pkg/devicetree/devicetree_windows.go
+++ pkg/devicetree/devicetree_windows.go
@@ -5,12 +5,69 @@ package devicetree
 import (
 	"bytes"
 	"encoding/asn1"
+	"encoding/binary"
 	"fmt"
 	"io"
 
 	"github.com/apex/log"
 	"github.com/blacktop/ipsw/internal/utils"
+	"github.com/blacktop/ipsw/pkg/img3"
 )
+
+// ParseImg3Data parses a img4 data containing a DeviceTree
+func ParseImg3Data(data []byte) (*DeviceTree, error) {
+
+	var i img3.Img3
+
+	r := bytes.NewReader(data)
+
+	if err := binary.Read(r, binary.LittleEndian, &i.Header); err != nil {
+		return nil, err
+	}
+
+	for {
+		var err error
+		var tag img3.Tag
+
+		err = binary.Read(r, binary.LittleEndian, &tag.TagHeader)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to read img3 tag header: %v", err)
+		}
+
+		tag.Data = make([]byte, tag.DataLength)
+		tag.Pad = make([]byte, tag.TotalLength-tag.DataLength-12)
+
+		err = binary.Read(r, binary.LittleEndian, &tag.Data)
+		err = binary.Read(r, binary.LittleEndian, &tag.Pad)
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to read img3 tag data: %v", err)
+		}
+
+		i.Tags = append(i.Tags, tag)
+	}
+
+	var dr io.Reader
+	if bytes.Contains(i.Tags[1].Data[:4], []byte("bvx2")) {
+		utils.Indent(log.Info, 2)("DeviceTree is LZFSE compressed")
+		return nil, fmt.Errorf("LZFSE decompression not supported on windows")
+	} else {
+		dr = bytes.NewReader(i.Tags[1].Data)
+	}
+
+	dtree, err := parseDeviceTree(dr)
+	if err != nil {
+		return nil, err
+	}
+
+	return dtree, nil
+}
 
 // ParseImg4Data parses a img4 data containing a DeviceTree
 func ParseImg4Data(data []byte) (*DeviceTree, error) {
