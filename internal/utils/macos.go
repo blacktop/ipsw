78f45961f6fae4587134825e56c600b09fe7a795
blacktop <blacktop@users.noreply.github.com>
2021-03-11 23:32:59 -0500

    feat: add 🆕 debugserver prep cmd

--- internal/utils/macos.go
+++ internal/utils/macos.go
@@ -5,7 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
-	"regexp"
+	"path/filepath"
 	"runtime"
 )
 
@@ -28,35 +28,71 @@ func Cp(src, dst string) error {
 	return err
 }
 
-// Mount mounts a DMG with hdiutil
-func Mount(image, mountPoint string) (string, error) {
+// CodeSign codesigns a given binary
+func CodeSign(filePath, signature string) error {
 	if runtime.GOOS == "darwin" {
-		var attachRe = regexp.MustCompile(`/dev/disk[\d]+`)
+		filePath = filepath.Clean(filePath)
+		cmd := exec.Command("codesign -s - -f", "-s", signature, "-f", filePath)
+
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("%v: %s", err, out)
+		}
+
+		return nil
+	}
+
+	return fmt.Errorf("only supported on macOS")
+}
+
+// CodeSignWithEntitlements codesigns a given binary with given entitlements
+func CodeSignWithEntitlements(filePath, entitlementsPath, signature string) error {
+	if runtime.GOOS == "darwin" {
+		cmd := exec.Command("codesign", "--entitlements", entitlementsPath, "-s", signature, "-f", filepath.Clean(filePath))
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("%v: %s", err, out)
+		}
+
+		return nil
+	}
+
+	return fmt.Errorf("only supported on macOS")
+}
+
+// CodeSignAdHoc codesigns a given binary with ad-hoc signature
+func CodeSignAdHoc(filePath string) error {
+	return CodeSign(filePath, "-")
+}
+
+// Mount mounts a DMG with hdiutil
+func Mount(image, mountPoint string) error {
+	if runtime.GOOS == "darwin" {
 		cmd := exec.Command("hdiutil", "attach", "-noverify", "-mountpoint", mountPoint, image)
 
 		out, err := cmd.CombinedOutput()
 		if err != nil {
-			return "", fmt.Errorf("%v: %s", err, out)
+			return fmt.Errorf("%v: %s", err, out)
 		}
 
-		return string(attachRe.Find(out)), nil
+		return nil
 	} else if runtime.GOOS == "linux" {
 		cmd := exec.Command("apfs-fuse", image, mountPoint)
 
 		out, err := cmd.CombinedOutput()
 		if err != nil {
-			return "", fmt.Errorf("%v: %s", err, out)
+			return fmt.Errorf("%v: %s", err, out)
 		}
-		return mountPoint, nil
+		return nil
 	}
 
-	return "", nil
+	return nil
 }
 
 // Unmount unmounts a DMG with hdiutil
-func Unmount(deviceNode string) error {
+func Unmount(mountPoint string) error {
 	if runtime.GOOS == "darwin" {
-		cmd := exec.Command("hdiutil", "detach", deviceNode)
+		cmd := exec.Command("hdiutil", "detach", mountPoint)
 
 		err := cmd.Run()
 		if err != nil {
@@ -66,7 +102,7 @@ func Unmount(deviceNode string) error {
 		return nil
 
 	} else if runtime.GOOS == "linux" {
-		cmd := exec.Command("umount", deviceNode)
+		cmd := exec.Command("umount", mountPoint)
 
 		err := cmd.Run()
 		if err != nil {
