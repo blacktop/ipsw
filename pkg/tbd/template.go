3f5e443448d8e00279c33823f57cd1087b2e0619
blacktop <blacktop@users.noreply.github.com>
2021-03-12 23:48:33 -0500

    feat: add 🆕dyld tbd cmd

--- pkg/tbd/template.go
+++ pkg/tbd/template.go
@@ -5,21 +5,8 @@ archs:           [ {{ StringsJoin .Archs ", " }} ]
 platform:        {{.Platform}}
 install-name:    {{.Path}}
 current-version: {{.Version}}
-exports:         
+exports:
   - archs:           [ {{ StringsJoin .Archs ", " }} ]
     symbols:         [ {{ StringsJoin .Symbols ", " }} ]
 ...
 `
-
-// const tbdTemplate = `---
-// archs:           [ {{ StringsJoin .Archs ", " }} ]
-// uuids:           [ {{ .Archs }}: {{ .UUID }} ]
-// platform:        {{.Platform}}
-// install-name:    {{.Path}}
-// current-version: {{.Version}}
-// exports:
-//   - archs:           [ {{ StringsJoin .Archs ", " }} ]
-//     symbols:         [ {{ StringsJoin .Symbols ", " }} ]
-//   objc-classes:    [  {{ StringsJoin .ObjcSyms ", " }}   end ]
-// ...
-// `
