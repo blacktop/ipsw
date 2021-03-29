86139ad5d96b38a33ce61b8cbbd98fab42c433df
blacktop <blacktop@users.noreply.github.com>
2021-03-28 22:15:58 -0400

    fix: dyld tbd symbol output

--- pkg/tbd/template.go
+++ pkg/tbd/template.go
@@ -1,12 +1,13 @@
 package tbd
 
-const tbdTemplate = `---
+const tbdTemplate = `--- !tapi-tbd-v3
 archs:           [ {{ StringsJoin .Archs ", " }} ]
 platform:        {{.Platform}}
 install-name:    {{.Path}}
 current-version: {{.Version}}
+objc-constraint: none
 exports:
   - archs:           [ {{ StringsJoin .Archs ", " }} ]
-    symbols:         [ {{ StringsJoin .Symbols ", " }} ]
+    symbols:         [ {{ StringsJoin .Symbols ",\n                       " }} ]
 ...
 `
