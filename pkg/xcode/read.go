f299f7b457faef0a7836744989cf1540f7155729
blacktop <blacktop@users.noreply.github.com>
2021-06-07 15:17:59 -0600

    feat: update device-list devices

--- pkg/xcode/read.go
+++ pkg/xcode/read.go
@@ -11,23 +11,40 @@ import (
 	"github.com/pkg/errors"
 )
 
+func uniqueDevices(d []Device) []Device {
+	unique := make(map[string]bool, len(d))
+	bs := make([]Device, len(unique))
+	for _, elem := range d {
+		if len(elem.ProductType) != 0 && len(elem.Target) != 0 {
+			if !unique[elem.ProductType+"_"+elem.Target] {
+				bs = append(bs, elem)
+				unique[elem.ProductType+"_"+elem.Target] = true
+			}
+		}
+	}
+
+	return bs
+}
+
 // ReadDeviceTraitsDB parse the XCode device_traits.db
 func ReadDeviceTraitsDB() ([]Device, error) {
 	var allDevices []Device
 
 	for _, osType := range []string{"iPhoneOS", "AppleTVOS", "WatchOS"} {
-		dbFile := fmt.Sprintf("/Applications/Xcode.app/Contents/Developer/Platforms/%s.platform/usr/standalone/device_traits.db", osType)
-		db, err := gorm.Open("sqlite3", dbFile)
-		if err != nil {
-			return nil, errors.Wrapf(err, "unable to open database: %s", dbFile)
+		for _, releaseType := range []string{"", "-beta"} {
+			dbFile := fmt.Sprintf("/Applications/Xcode%s.app/Contents/Developer/Platforms/%s.platform/usr/standalone/device_traits.db", releaseType, osType)
+			db, err := gorm.Open("sqlite3", dbFile)
+			if err != nil {
+				return nil, errors.Wrapf(err, "unable to open database: %s", dbFile)
+			}
+			defer db.Close()
+
+			var devices []Device
+			db.Preload("DeviceTrait").Find(&devices)
+
+			allDevices = append(allDevices, devices...)
 		}
-		defer db.Close()
-
-		var devices []Device
-		db.Preload("DeviceTrait").Find(&devices)
-
-		allDevices = append(allDevices, devices...)
 	}
 
-	return allDevices, nil
+	return uniqueDevices(allDevices), nil
 }
