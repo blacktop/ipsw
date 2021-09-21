ea52428ff65b95398ca031d9c3e9640dbb1c892a
blacktop <blacktop@users.noreply.github.com>
2021-09-20 21:23:23 -0600

    feat: add xcode path flag to dyld split cmd

--- cmd/ipsw/cmd/dyld_split.go
+++ cmd/ipsw/cmd/dyld_split.go
@@ -1,3 +1,4 @@
+//go:build darwin && cgo
 // +build darwin,cgo
 
 package cmd
@@ -7,66 +8,20 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
 
 	"github.com/apex/log"
 	"github.com/blacktop/ipsw/pkg/dyld"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
-
-var deviceOsFlag string
 
 func init() {
 	dyldCmd.AddCommand(splitCmd)
 
-	splitCmd.Flags().BoolP("iphone", "i", true, "Extract from iPhoneOS cache")
-	splitCmd.Flags().BoolP("appletv", "a", false, "Extract from AppleTVOS cache")
-	splitCmd.Flags().BoolP("watch", "w", false, "Extract from WatchOS cache")
+	splitCmd.Flags().StringP("xcode", "x", viper.GetString("IPSW_XCODE_PATH"), "Path to Xcode.app")
 
 	splitCmd.MarkZshCompPositionalArgumentFile(1, "dyld_shared_cache*")
-}
-
-func contains(slice []string, item string) bool {
-	set := make(map[string]struct{}, len(slice))
-	for _, s := range slice {
-		set[s] = struct{}{}
-	}
-
-	_, ok := set[item]
-	return ok
-}
-
-func checkRequiredFlags(flags *pflag.FlagSet) error {
-	requiredError := false
-	flagSetCount := 0
-
-	flags.VisitAll(func(flag *pflag.Flag) {
-		if contains([]string{"iphone", "appletv", "watch"}, flag.Name) {
-			if strings.EqualFold(flag.Value.String(), "true") {
-				flagSetCount++
-				switch flag.Name {
-				case "iphone":
-					deviceOsFlag = "iPhoneOS"
-				case "appletv":
-					deviceOsFlag = "AppleTVOS"
-				case "watch":
-					deviceOsFlag = "WatchOS"
-				}
-			}
-
-			if flagSetCount > 1 {
-				requiredError = true
-			}
-		}
-	})
-
-	if requiredError {
-		return errors.New("Flags are mutually exclusive: please only set one of -i|-a|-w")
-	}
-
-	return nil
 }
 
 // splitCmd represents the split command
@@ -74,25 +29,15 @@ var splitCmd = &cobra.Command{
 	Use:   "split <dyld_shared_cache> <optional_output_path>",
 	Short: "Extracts all the dyld_shared_cache libraries",
 	Args:  cobra.MinimumNArgs(1),
-	PreRunE: func(cmd *cobra.Command, args []string) error {
-		return checkRequiredFlags(cmd.Flags())
-	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 
 		if Verbose {
 			log.SetLevel(log.DebugLevel)
 		}
 
-		var outputPath string
+		xcodePath, _ := cmd.Flags().GetString("xcode")
 
 		dscPath := filepath.Clean(args[0])
-
-		if len(args) > 1 {
-			outputPath = filepath.Clean(args[1])
-			if _, err := os.Stat(outputPath); os.IsNotExist(err) {
-				return fmt.Errorf("path %s does not exist", dscPath)
-			}
-		}
 
 		fileInfo, err := os.Lstat(dscPath)
 		if err != nil {
@@ -116,12 +61,16 @@ var splitCmd = &cobra.Command{
 			log.Fatal("dyld_shared_cache splitting only works on macOS")
 		}
 
-		if len(outputPath) > 0 {
-			fullPath, _ := filepath.Abs(outputPath)
-			log.Infof("Splitting dyld_shared_cache to %s\n", fullPath)
-			return dyld.Split(dscPath, outputPath, deviceOsFlag)
+		if len(args) > 1 {
+			outputPath, _ := filepath.Abs(filepath.Clean(args[1]))
+			if _, err := os.Stat(outputPath); os.IsNotExist(err) {
+				return fmt.Errorf("path %s does not exist", dscPath)
+			}
+			log.Infof("Splitting dyld_shared_cache to %s\n", outputPath)
+			return dyld.Split(dscPath, outputPath, xcodePath)
 		}
+
 		log.Info("Splitting dyld_shared_cache")
-		return dyld.Split(dscPath, filepath.Dir(dscPath), deviceOsFlag)
+		return dyld.Split(dscPath, filepath.Dir(dscPath), xcodePath)
 	},
 }
