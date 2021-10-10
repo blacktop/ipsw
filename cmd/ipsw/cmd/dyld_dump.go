cb71d927ac6e23254737e6abc2a8cbc21c59939e
blacktop <blacktop@users.noreply.github.com>
2021-10-09 21:32:06 -0600

    feat: add dyld dump cmd

---
+++ cmd/ipsw/cmd/dyld_dump.go
@@ -0,0 +1,140 @@
+/*
+Copyright © 2021 blacktop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/apex/log"
+	"github.com/blacktop/ipsw/internal/utils"
+	"github.com/blacktop/ipsw/pkg/dyld"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	dyldCmd.AddCommand(dyldDumpCmd)
+
+	dyldDumpCmd.Flags().BoolP("addr", "a", false, "Output as addresses/uint64s")
+	dyldDumpCmd.Flags().BoolP("hex", "x", false, "Output as hexdump")
+	dyldDumpCmd.Flags().Uint64P("size", "s", 0, "Size of data in bytes")
+	dyldDumpCmd.Flags().Uint64P("count", "c", 0, "The number of total items to display")
+}
+
+// dyldDumpCmd represents the dump command
+var dyldDumpCmd = &cobra.Command{
+	Use:   "dump",
+	Short: "Dump dyld_shared_cache data at given offset",
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if Verbose {
+			log.SetLevel(log.DebugLevel)
+		}
+
+		size, _ := cmd.Flags().GetUint64("size")
+		count, _ := cmd.Flags().GetUint64("count")
+
+		asAddrs, _ := cmd.Flags().GetBool("addr")
+		asHex, _ := cmd.Flags().GetBool("hex")
+
+		if size > 0 && count > 0 {
+			return fmt.Errorf("you can only use --size OR --count")
+		}
+
+		if asAddrs && asHex {
+			return fmt.Errorf("you can only use --addr OR --hex")
+		} else if !asAddrs && !asHex {
+			asHex = true
+			if size == 0 && count == 0 {
+				size = 0x100
+			}
+		}
+
+		addr, err := utils.ConvertStrToInt(args[1])
+		if err != nil {
+			return err
+		}
+
+		if asAddrs && size == 0 {
+			size = count * uint64(binary.Size(uint64(0)))
+		}
+
+		dscPath := filepath.Clean(args[0])
+
+		fileInfo, err := os.Lstat(dscPath)
+		if err != nil {
+			return fmt.Errorf("file %s does not exist", dscPath)
+		}
+
+		// Check if file is a symlink
+		if fileInfo.Mode()&os.ModeSymlink != 0 {
+			symlinkPath, err := os.Readlink(dscPath)
+			if err != nil {
+				return errors.Wrapf(err, "failed to read symlink %s", dscPath)
+			}
+			// TODO: this seems like it would break
+			linkParent := filepath.Dir(dscPath)
+			linkRoot := filepath.Dir(linkParent)
+
+			dscPath = filepath.Join(linkRoot, symlinkPath)
+		}
+
+		f, err := dyld.Open(dscPath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		uuid, off, err := f.GetOffset(addr)
+		if err != nil {
+			log.Error(err.Error())
+		} else {
+			dat, err := f.ReadBytesForUUID(uuid, int64(off), size)
+			if err != nil {
+				return err
+			}
+
+			if asHex {
+				fmt.Println(hex.Dump(dat))
+			} else if asAddrs {
+				if count == 0 {
+					count = size / uint64(binary.Size(uint64(0)))
+				}
+				addrs := make([]uint64, count)
+				if err := binary.Read(bytes.NewReader(dat), f.ByteOrder, addrs); err != nil {
+					return err
+				}
+				for _, a := range addrs {
+					fmt.Printf("%#x\n", f.SlideInfo.SlidePointer(a))
+				}
+			}
+		}
+
+		return nil
+	},
+}
