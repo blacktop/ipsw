0b49d687d20624aeaec1fc88691a3d56269558ca
blacktop <blacktop@users.noreply.github.com>
2021-10-27 22:39:55 -0600

    feat: add 🆕 download rss cmd  (#44)

---
+++ cmd/ipsw/cmd/download_rss.go
@@ -0,0 +1,98 @@
+/*
+Copyright © 2021 blacktop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+	"time"
+
+	"github.com/apex/log"
+	"github.com/blacktop/ipsw/internal/download"
+	"github.com/blacktop/ipsw/internal/utils"
+	"github.com/gen2brain/beeep"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	downloadCmd.AddCommand(rssCmd)
+
+	rssCmd.Flags().BoolP("watch", "w", false, "Watch for NEW releases")
+}
+
+// rssCmd represents the rss command
+var rssCmd = &cobra.Command{
+	Use:   "rss",
+	Short: "Read Releases - Apple Developer RSS Feed",
+	Run: func(cmd *cobra.Command, args []string) {
+		var releases []string
+
+		if Verbose {
+			log.SetLevel(log.DebugLevel)
+		}
+
+		watch, _ := cmd.Flags().GetBool("watch")
+
+		rss, err := download.GetRSS()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		for _, item := range rss.Channel.Items {
+			releases = append(releases, fmt.Sprintf("%s - %s", item.Title, item.PubDate))
+		}
+
+		if watch {
+			log.Info("Watching Releases - Apple Developer RSS Feed...")
+			for {
+				time.Sleep(5 * time.Minute)
+
+				// check for NEW releases
+				rss, err := download.GetRSS()
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+
+				for _, item := range rss.Channel.Items {
+					if !utils.StrSliceContains(releases, fmt.Sprintf("%s - %s", item.Title, item.PubDate)) {
+
+						releases = append(releases, fmt.Sprintf("%s - %s", item.Title, item.PubDate))
+
+						if err := beeep.Alert("🆕 Apple - Release", fmt.Sprintf("%s - %s", item.Title, item.PubDate), "assets/warning.png"); err != nil {
+							log.Fatal(err.Error())
+						}
+					}
+				}
+			}
+		}
+
+		// Dump the Feed
+		fmt.Printf("# %s (%s)\n\n", rss.Channel.Title, rss.Channel.Link)
+		fmt.Printf("> %s  \n\n---\n\n", rss.Channel.Desc)
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+		for _, item := range rss.Channel.Items {
+			fmt.Fprintf(w, "- %s\t<%s>\t%s  \n", item.Title, item.PubDate, item.Link)
+		}
+		w.Flush()
+		fmt.Println()
+	},
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.17
 require (
 	github.com/AlecAivazis/survey/v2 v2.3.2
 	github.com/Netflix/go-expect v0.0.0-20211003183012-e1a7c020ce25 // indirect
-	github.com/PuerkitoBio/goquery v1.7.1
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/apex/log v1.9.0
 	github.com/blacktop/go-arm64 v0.6.18
 	github.com/blacktop/go-macho v1.1.56
@@ -13,7 +13,7 @@ require (
 	github.com/blacktop/lzss v0.1.1
 	github.com/blacktop/ranger v1.0.3
 	github.com/dustin/go-humanize v1.0.0
-	github.com/fatih/color v1.13.0 // indirect
+	github.com/fatih/color v1.13.0
 	github.com/fullsailor/pkcs7 v0.0.0-20190404230743-d7302db945fa
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/hashicorp/go-version v1.3.0
@@ -29,10 +29,12 @@ require (
 	github.com/therootcompany/xz v1.0.1
 	github.com/vbauerster/mpb/v5 v5.4.0
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
-	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359
 )
+
+require github.com/gen2brain/beeep v0.0.0-20210529141713-5586760f0cc1
 
 require (
 	github.com/VividCortex/ewma v1.2.0 // indirect
@@ -42,9 +44,13 @@ require (
 	github.com/antchfx/xmlquery v1.3.8 // indirect
 	github.com/antchfx/xpath v1.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/godbus/dbus/v5 v5.0.6 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20211023200351-1e6abe791855 // indirect
+	github.com/gopherjs/gopherwasm v1.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -56,6 +62,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
@@ -64,6 +71,7 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
