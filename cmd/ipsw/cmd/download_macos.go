896b4bf096402477fa28c06bebfc435750839305
blacktop <blacktop@users.noreply.github.com>
2021-11-02 18:11:02 -0600

    fix: download macos cmd (now downloads/builds macOS installers)

--- cmd/ipsw/cmd/download_macos.go
+++ cmd/ipsw/cmd/download_macos.go
@@ -23,7 +23,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
 	"github.com/apex/log"
 	"github.com/blacktop/ipsw/internal/download"
 	"github.com/spf13/cobra"
@@ -33,15 +36,20 @@ import (
 func init() {
 	downloadCmd.AddCommand(macosCmd)
 
-	macosCmd.Flags().BoolP("installer", "", false, "Show latest macOS installers")
-	// macosCmd.Flags().StringP("work-dir", "w", "", "macOS installer creator working directory")
-	macosCmd.Flags().BoolP("kernel", "k", false, "Extract kernelcache from remote installer")
+	macosCmd.Flags().BoolP("list", "l", false, "Show latest macOS installers")
+	macosCmd.Flags().StringP("work-dir", "w", "", "macOS installer creator working directory")
+	// macosCmd.Flags().BoolP("kernel", "k", false, "Extract kernelcache from remote installer")
+	viper.BindPFlag("download.macos.list", macosCmd.Flags().Lookup("list"))
+	viper.BindPFlag("download.macos.work-dir", macosCmd.Flags().Lookup("work-dir"))
+	// viper.BindPFlag("download.macos.kernel", macosCmd.Flags().Lookup("kernel"))
 }
 
 // macosCmd represents the macos command
 var macosCmd = &cobra.Command{
-	Use:   "macos",
-	Short: "Download macOS installers",
+	Use:           "macos",
+	Short:         "Download macOS installers",
+	SilenceUsage:  false,
+	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
 		if Verbose {
@@ -54,7 +62,7 @@ var macosCmd = &cobra.Command{
 		viper.BindPFlag("download.skip-all", cmd.Flags().Lookup("skip-all"))
 		viper.BindPFlag("download.resume-all", cmd.Flags().Lookup("resume-all"))
 		viper.BindPFlag("download.restart-all", cmd.Flags().Lookup("restart-all"))
-		viper.BindPFlag("download.remove-commas", cmd.Flags().Lookup("remove-commas"))
+		// viper.BindPFlag("download.remove-commas", cmd.Flags().Lookup("remove-commas"))
 		viper.BindPFlag("download.white-list", cmd.Flags().Lookup("white-list"))
 		viper.BindPFlag("download.black-list", cmd.Flags().Lookup("black-list"))
 		viper.BindPFlag("download.device", cmd.Flags().Lookup("device"))
@@ -62,46 +70,92 @@ var macosCmd = &cobra.Command{
 		viper.BindPFlag("download.version", cmd.Flags().Lookup("version"))
 		viper.BindPFlag("download.build", cmd.Flags().Lookup("build"))
 
+		cmd.Flags().Lookup("remove-commas").Hidden = true
+
 		// settings
-		// proxy := viper.GetString("download.proxy")
-		// insecure := viper.GetBool("download.insecure")
-		// confirm := viper.GetBool("download.confirm")
-		// skipAll := viper.GetBool("download.skip-all")
-		// resumeAll := viper.GetBool("download.resume-all")
-		// restartAll := viper.GetBool("download.restart-all")
-		// removeCommas := viper.GetBool("download.remove-commas")
+		proxy := viper.GetString("download.proxy")
+		insecure := viper.GetBool("download.insecure")
+		confirm := viper.GetBool("download.confirm")
+		skipAll := viper.GetBool("download.skip-all")
+		resumeAll := viper.GetBool("download.resume-all")
+		restartAll := viper.GetBool("download.restart-all")
 		// filters
-		// device := viper.GetString("download.device")
-		// model := viper.GetString("download.model")
-		// version := viper.GetString("download.version")
-		// build := viper.GetString("download.build")
-		// doDownload := viper.GetStringSlice("download.white-list")
-		// doNotDownload := viper.GetStringSlice("download.black-list")
+		version := viper.GetString("download.version")
+		build := viper.GetString("download.build")
+		// flags
+		showInstallers := viper.GetBool("download.macos.list")
+		workDir := viper.GetString("download.macos.work-dir")
+		// remoteKernel := viper.GetString("download.macos.kernel")
 
-		showInstallers, _ := cmd.Flags().GetBool("installer")
-		// workDir, _ := cmd.Flags().GetString("work-dir")
-		// remoteKernel, _ := cmd.Flags().GetBool("kernel")
+		// verify args
+		if len(version) > 0 && len(build) > 0 {
+			return fmt.Errorf("you cannot supply a --version AND a --build (they are mutually exclusive)")
+		}
 
-		// var destPath string
-		// if len(args) > 0 {
-		// 	destPath = filepath.Clean(args[0])
-		// }
+		prods, err := download.GetProductInfo()
+		if err != nil {
+			return err
+		}
 
 		if showInstallers {
-			if prods, err := download.GetProductInfo(); err != nil {
-				log.Error(err.Error())
-			} else {
-				fmt.Println(prods)
-				// for _, prod := range prods {
-				// 	// if prod.ProductID == "071-14766" {
-				// 	if prod.ProductID == "001-68446" {
-				// 		if err := prod.DownloadInstaller(workDir, proxy, insecure, skipAll); err != nil {
-				// 			log.Error(err.Error())
-				// 		}
-				// 	}
-				// }
-			}
+			fmt.Println(prods)
 			return nil
+		}
+
+		if len(version) > 0 {
+			prods = prods.FilterByVersion(version)
+		}
+
+		var prodList []string
+		for _, p := range prods {
+			prodList = append(prodList, fmt.Sprintf("%-35s%-8s - %-8s - %s", p.Title, p.Version, p.Build, p.PostDate.Format("01Jan06 15:04:05")))
+		}
+
+		if len(prodList) == 0 {
+			return fmt.Errorf("no installers found for given options")
+		}
+
+		var prod download.ProductInfo
+		if len(prodList) > 1 && len(build) == 0 {
+			choice := 0
+			prompt := &survey.Select{
+				Message:  "Choose an installer:",
+				Options:  prodList,
+				PageSize: 20,
+			}
+			if err := survey.AskOne(prompt, &choice); err != nil {
+				if err == terminal.InterruptErr {
+					log.Warn("Exiting...")
+					os.Exit(0)
+				}
+				log.Fatal(err.Error())
+			}
+			prod = prods[choice]
+		} else {
+			for _, p := range prods {
+				if version == p.Version || build == p.Build {
+					prod = p
+				}
+			}
+		}
+
+		cont := true
+		if !confirm {
+			prompt := &survey.Confirm{
+				Message: fmt.Sprintf("You are about to download the %s installer files. Continue?", prod.Title),
+			}
+			if err := survey.AskOne(prompt, &cont); err != nil {
+				if err == terminal.InterruptErr {
+					log.Warn("Exiting...")
+					os.Exit(0)
+				}
+				log.Fatal(err.Error())
+			}
+		}
+		if cont {
+			if err := prod.DownloadInstaller(workDir, proxy, insecure, skipAll, resumeAll, restartAll); err != nil {
+				return err
+			}
 		}
 
 		return nil
