71d07b09011cc82360d04c1396e2aa7cbe690366
blacktop <blacktop@users.noreply.github.com>
2022-02-01 22:25:38 -0700

    feat: add ability to pull all shared_cache arches via dyld `extract` cmds

--- cmd/ipsw/cmd/dyld_extract.go
+++ cmd/ipsw/cmd/dyld_extract.go
@@ -29,6 +29,7 @@ import (
 	"path/filepath"
 
 	"github.com/apex/log"
+	"github.com/blacktop/ipsw/internal/utils"
 	"github.com/blacktop/ipsw/pkg/dyld"
 	"github.com/blacktop/ipsw/pkg/info"
 	"github.com/spf13/cobra"
@@ -36,18 +37,30 @@ import (
 
 func init() {
 	dyldCmd.AddCommand(extractDyldCmd)
-
+	extractDyldCmd.Flags().StringArrayP("dyld-arch", "a", []string{}, "dyld_shared_cache architecture to extract")
 	extractDyldCmd.MarkZshCompPositionalArgumentFile(1, "*.ipsw")
 }
 
 // extractDyldCmd represents the extractDyld command
 var extractDyldCmd = &cobra.Command{
-	Use:   "extract <IPSW> <DEST>",
-	Short: "Extract dyld_shared_cache from DMG in IPSW",
-	Args:  cobra.MinimumNArgs(1),
+	Use:           "extract <IPSW> <DEST>",
+	Short:         "Extract dyld_shared_cache from DMG in IPSW",
+	Args:          cobra.MinimumNArgs(1),
+	SilenceUsage:  true,
+	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if Verbose {
 			log.SetLevel(log.DebugLevel)
+		}
+
+		dyldArches, _ := cmd.Flags().GetStringArray("dyld-arch")
+
+		if len(dyldArches) > 0 {
+			for _, arch := range dyldArches {
+				if !utils.StrSliceHas([]string{"arm64", "arm64e", "x86_64", "x86_64h"}, arch) {
+					return fmt.Errorf("invalid architecture: %s (must be: arm64, arm64e, x86_64 or x86_64h)", arch)
+				}
+			}
 		}
 
 		ipswPath := filepath.Clean(args[0])
@@ -69,6 +82,6 @@ var extractDyldCmd = &cobra.Command{
 		destPath = filepath.Join(destPath, i.GetFolder())
 
 		log.Info("Extracting dyld_shared_cache")
-		return dyld.Extract(ipswPath, destPath)
+		return dyld.Extract(ipswPath, destPath, dyldArches)
 	},
 }
