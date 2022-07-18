6a8ce7032dbba01c615172e1a22e0975e2dceab5
blacktop <blacktop@users.noreply.github.com>
2022-07-18 18:58:13 +0100

    fix: detecting existence of sub-cache extension ascii in non-stub-island NEW style DSCs

--- pkg/dyld/file.go
+++ pkg/dyld/file.go
@@ -609,12 +609,13 @@ func (f *File) parseCache(r io.ReaderAt, uuid mtypes.UUID) error {
 	if f.Headers[uuid].SubCacheArrayCount > 0 && f.Headers[uuid].SubCacheArrayOffset > 0 {
 		sr.Seek(int64(f.Headers[uuid].SubCacheArrayOffset), io.SeekStart)
 		f.SubCacheInfo = make([]SubcacheEntryPageInLink, f.Headers[uuid].SubCacheArrayCount)
-		if f.Headers[f.UUID].CacheType == CacheTypeStubIslands {
+		// TODO: gross hack to read the subcache info for pre iOS 16.
+		endOfSubCacheInfoArray := f.Headers[f.UUID].SubCacheArrayOffset + uint32(binary.Size(SubcacheEntryPageInLink{})*int(f.Headers[f.UUID].SubCacheArrayCount))
+		if endOfSubCacheInfoArray == f.Images[0].PathOffset {
 			if err := binary.Read(sr, f.ByteOrder, f.SubCacheInfo); err != nil {
 				return err
 			}
 		} else {
-			// TODO: gross hack to read the subcache info for pre iOS 16.
 			subCacheInfo := make([]SubcacheEntry, f.Headers[uuid].SubCacheArrayCount)
 			if err := binary.Read(sr, f.ByteOrder, subCacheInfo); err != nil {
 				return err
