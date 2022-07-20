ae6adeebe4bbc25db786b3f9955f481be9ba8fc0
blacktop <blacktop@users.noreply.github.com>
2022-07-20 14:04:06 +0100

    fix: kc version for fileset kernels

--- pkg/kernelcache/kernelcache.go
+++ pkg/kernelcache/kernelcache.go
@@ -10,7 +10,9 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/apex/log"
 	// lzfse "github.com/blacktop/go-lzfse"
@@ -37,6 +39,28 @@ type CompressedCache struct {
 	Header interface{}
 	Size   int
 	Data   []byte
+}
+
+type Version struct {
+	Kernel struct {
+		Darwin string    `json:"darwin,omitempty"`
+		Date   time.Time `json:"date,omitempty"`
+		XNU    string    `json:"xnu,omitempty"`
+		Type   string    `json:"type,omitempty"`
+		Arch   string    `json:"arch,omitempty"`
+		CPU    string    `json:"cpu,omitempty"`
+	} `json:"kernel,omitempty"`
+	LLVM struct {
+		Version string   `json:"version,omitempty"`
+		Clang   string   `json:"clang,omitempty"`
+		Flags   []string `json:"flags,omitempty"`
+	} `json:"llvm,omitempty"`
+	rawKernel string
+	rawLLVM   string
+}
+
+func (v *Version) String() string {
+	return fmt.Sprintf("%s\n%s", v.rawKernel, v.rawLLVM)
 }
 
 // ParseImg4Data parses a img4 data containing a compressed kernelcache.
@@ -266,8 +290,7 @@ func RemoteParse(zr *zip.Reader, destPath string) error {
 func Parse(r io.ReadCloser) ([]byte, error) {
 	var buf bytes.Buffer
 
-	_, err := r.Read(buf.Bytes())
-	if err != nil {
+	if _, err := r.Read(buf.Bytes()); err != nil {
 		return nil, errors.Wrap(err, "failed to read data")
 	}
 
@@ -283,4 +306,83 @@ func Parse(r io.ReadCloser) ([]byte, error) {
 	r.Close()
 
 	return dec, nil
+}
+
+func GetVersion(m *macho.File) (*Version, error) {
+	var kv Version
+
+	kc := m
+
+	if kc.FileTOC.FileHeader.Type == types.FileSet {
+		var err error
+		kc, err = m.GetFileSetFileByName("kernel")
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse fileset entry 'kernel': %v", err)
+		}
+	}
+
+	if sec := kc.Section("__TEXT", "__const"); sec != nil {
+		dat, err := sec.Data()
+		if err != nil {
+			return nil, fmt.Errorf("failed to read cstrings in %s.%s: %v", sec.Seg, sec.Name, err)
+		}
+
+		csr := bytes.NewBuffer(dat[:])
+
+		foundKV := false
+		foundLLVM := false
+
+		for {
+			s, err := csr.ReadString('\x00')
+
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				return nil, fmt.Errorf("failed to read string: %v", err)
+			}
+
+			s = strings.Trim(s, "\x00")
+
+			if len(s) > 0 {
+				if utils.IsASCII(s) {
+					reKV := regexp.MustCompile(`^Darwin Kernel Version (?P<darwin>.+): (?P<date>.+); root:xnu-(?P<xnu>.+)/(?P<type>.+)_(?P<arch>.+)_(?P<cpu>.+)$`)
+					if reKV.MatchString(s) {
+						foundKV = true
+						kv.rawKernel = s
+						matches := reKV.FindStringSubmatch(s)
+						kv.Kernel.Darwin = matches[reKV.SubexpIndex("darwin")]
+						// TODO: confirm that day is not in form 02 for day
+						kv.Kernel.Date, err = time.Parse("Mon Jan 2 15:04:05 MST 2006", matches[reKV.SubexpIndex("date")])
+						if err != nil {
+							return nil, fmt.Errorf("failed to parse date %s: %v", matches[reKV.SubexpIndex("date")], err)
+						}
+						kv.Kernel.XNU = matches[reKV.SubexpIndex("xnu")]
+						kv.Kernel.Type = matches[reKV.SubexpIndex("type")]
+						kv.Kernel.Arch = matches[reKV.SubexpIndex("arch")]
+						kv.Kernel.CPU = matches[reKV.SubexpIndex("cpu")]
+					}
+
+					reLLVM := regexp.MustCompile(`^Apple LLVM (?P<version>.+) \(clang-(?P<clang>.+)\) \[(?P<flags>.+)\]$`)
+					if reLLVM.MatchString(s) {
+						foundLLVM = true
+						kv.rawLLVM = s
+						matches := reLLVM.FindStringSubmatch(s)
+						kv.LLVM.Version = matches[reLLVM.SubexpIndex("version")]
+						kv.LLVM.Clang = matches[reLLVM.SubexpIndex("clang")]
+						kv.LLVM.Flags = strings.Split(matches[reLLVM.SubexpIndex("flags")], ", ")
+					}
+
+					if foundKV && foundLLVM {
+						break
+					}
+				}
+			}
+		}
+	} else {
+		return nil, fmt.Errorf("section __TEXT.__const not found in kernelcache (if this is a macOS kernel you might need to first extract the fileset entry)")
+	}
+
+	return &kv, nil
 }
