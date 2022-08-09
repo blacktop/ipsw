7d1e0f859e32ef84b4dac16d2d765f38f7f4e3d8
blacktop <blacktop@users.noreply.github.com>
2022-08-08 19:07:10 -0600

    fix: `ipsw extract --files --pattern` cmd 🐛

--- internal/utils/macos.go
+++ internal/utils/macos.go
@@ -35,6 +35,65 @@ func Cp(src, dst string) error {
 	return err
 }
 
+// Copy recursively copies src into dst with src's file modes.
+// CREDIT: https://github.com/goreleaser/goreleaser/blob/main/internal/gio/copy.go
+func Copy(src, dst string) error {
+	return CopyWithMode(src, dst, 0)
+}
+
+// CopyWithMode recursively copies src into dst with the given mode.
+// The given mode applies only to files. Their parent dirs will have the same mode as their src counterparts.
+func CopyWithMode(src, dst string, mode os.FileMode) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("failed to copy %s to %s: %w", src, dst, err)
+		}
+		// We have the following:
+		// - src = "a/b"
+		// - dst = "dist/linuxamd64/b"
+		// - path = "a/b/c.txt"
+		// So we join "a/b" with "c.txt" and use it as the destination.
+		dst := filepath.Join(dst, strings.Replace(path, src, "", 1))
+		if info.IsDir() {
+			return os.MkdirAll(dst, info.Mode())
+		}
+		if info.Mode()&os.ModeSymlink != 0 {
+			return copySymlink(path, dst)
+		}
+		if mode != 0 {
+			return copyFile(path, dst, mode)
+		}
+		return copyFile(path, dst, info.Mode())
+	})
+}
+
+func copySymlink(src, dst string) error {
+	src, err := os.Readlink(src)
+	if err != nil {
+		return err
+	}
+	return os.Symlink(src, dst)
+}
+
+func copyFile(src, dst string, mode os.FileMode) error {
+	original, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("failed to open '%s': %w", src, err)
+	}
+	defer original.Close()
+
+	new, err := os.OpenFile(dst, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, mode)
+	if err != nil {
+		return fmt.Errorf("failed to open '%s': %w", dst, err)
+	}
+	defer new.Close()
+
+	if _, err := io.Copy(new, original); err != nil {
+		return fmt.Errorf("failed to copy: %w", err)
+	}
+	return nil
+}
+
 // CodeSign codesigns a given binary
 func CodeSign(filePath, signature string) error {
 	if runtime.GOOS == "darwin" {
@@ -372,13 +431,14 @@ func ExtractFromDMG(dmgPath, destPath string, pattern *regexp.Regexp) error {
 		if info.IsDir() {
 			return nil
 		}
-		if pattern.MatchString(info.Name()) {
+		if pattern.MatchString(strings.TrimPrefix(path, mountPoint)) {
 			fname := strings.TrimPrefix(path, mountPoint)
-			if err := os.MkdirAll(filepath.Join(destPath, filepath.Dir(fname)), 0750); err != nil {
+			fname = filepath.Join(destPath, fname)
+			if err := os.MkdirAll(filepath.Dir(fname), 0750); err != nil {
 				return fmt.Errorf("failed to create directory %s: %v", filepath.Join(destPath, filepath.Dir(fname)), err)
 			}
-			Indent(log.Info, 3)(fmt.Sprintf("Extracting %s", fname))
-			if err := Cp(path, filepath.Join(destPath, fname)); err != nil {
+			Indent(log.Info, 3)(fmt.Sprintf("Extracting to %s", fname))
+			if err := Copy(path, fname); err != nil {
 				return fmt.Errorf("failed to extract %s: %v", fname, err)
 			}
 		}
