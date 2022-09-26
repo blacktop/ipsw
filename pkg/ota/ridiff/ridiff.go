0ccd1f1a5c0318483f717ba1b553345029089c51
blacktop <blacktop@users.noreply.github.com>
2022-09-25 19:29:21 -0600

    feat: add 🆕 RIDIFF10 cryptex parser (macOS only) 😞

---
+++ pkg/ota/ridiff/ridiff.go
@@ -0,0 +1,86 @@
+//go:build darwin && cgo
+
+package ridiff
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+typedef struct
+{
+    int64_t unknown1;
+    int64_t unknown2;
+    char *input;
+    char *output;
+    char *patch;
+    uint32_t not_cryptex_cache;
+    uint32_t threads; // 0 means use all hw.physicalcpu
+    uint32_t verbose;
+} RawImage;
+
+typedef int64_t RawImagePatch(RawImage *);
+int ParseRawImage(char *output, char *patch);
+
+int ParseRawImage(char *output, char *patch) {
+	void *handle = dlopen("/usr/lib/libParallelCompression.dylib", RTLD_LAZY);
+	if (!handle) {
+		fprintf(stderr, "%s\n", dlerror());
+		return 1;
+	}
+
+	RawImagePatch *RawImagePatch = dlsym(handle, "RawImagePatch");
+	if (!RawImagePatch) {
+		fprintf(stderr, "%s\n", dlerror());
+		return 1;
+	}
+
+    RawImage ri = {
+        .unknown1 = 0,
+        .unknown2 = 0,
+        .input = NULL,
+        .output = output,
+        .patch = patch,
+        .not_cryptex_cache = 0,
+        .threads = 0,
+        .verbose = 0,
+    };
+
+	int64_t ret = RawImagePatch(&ri);
+	if (ret != 0) {
+		fprintf(stderr, "RawImagePatch returned %lld", ret);
+		return 1;
+	}
+
+	if (dlclose(handle) != 0) {
+		fprintf(stderr, "dlclose failed: %s\n", dlerror());
+		return 1;
+	}
+
+	return 0;
+}
+
+*/
+import "C"
+import (
+	"errors"
+	"fmt"
+	"unsafe"
+)
+
+// RawImagePatch takes a RawImage struct and returns a pointer to the symbol.
+func RawImagePatch(patch, output string) error {
+
+	p := C.CString(patch)
+	defer C.free(unsafe.Pointer(p))
+
+	o := C.CString(output)
+	defer C.free(unsafe.Pointer(o))
+
+	ret := C.ParseRawImage(o, p)
+	if ret != 0 {
+		return fmt.Errorf("error parsing raw image: %v", errors.New(C.GoString(C.dlerror())))
+	}
+
+	return nil
+}
