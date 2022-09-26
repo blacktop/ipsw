d81ef131264e7b7a65227455710780dd8798d7bf
blacktop <blacktop@users.noreply.github.com>
2022-09-25 20:55:17 -0600

    Update ridiff_other.go

--- pkg/ota/ridiff/ridiff_other.go
+++ pkg/ota/ridiff/ridiff_other.go
@@ -2,6 +2,8 @@
 
 package ridiff
 
+import "fmt"
+
 // RawImagePatch takes a Raw Image Diff and converts it to an APFS volume.
 func RawImagePatch(patch, output string) error {
 	return fmt.Errorf("RawImagePatch: only supported on darwin")
