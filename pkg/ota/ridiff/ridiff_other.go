f4076876665ed3b4cc8a1f7fb32f3698d32aef2d
blacktop <blacktop@users.noreply.github.com>
2022-09-25 20:45:51 -0600

    feat: `ipsw download ota --dyld` to download DSC w/o having to download the entire OTA working again on iOS16.x/macOS13.x (requires macOS 13 to run)

---
+++ pkg/ota/ridiff/ridiff_other.go
@@ -0,0 +1,8 @@
+//go:build !darwin
+
+package ridiff
+
+// RawImagePatch takes a Raw Image Diff and converts it to an APFS volume.
+func RawImagePatch(patch, output string) error {
+	return fmt.Errorf("RawImagePatch: only supported on darwin")
+}
