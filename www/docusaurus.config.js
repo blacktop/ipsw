8978f834c45d0f9696ea75c80671ef945a1053a6
blacktop <blacktop@users.noreply.github.com>
2023-01-16 18:08:44 -0700

    Update docusaurus.config.js

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
         themes: [path.resolve(__dirname, "/src/themes/prism-nord")],
-        additionalLanguages: ["armasm", "llvm", "objc"],
+        additionalLanguages: ["armasm", "llvm"],
       },
       algolia: {
         appId: "XN7OVST81R",
