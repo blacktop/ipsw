aeeddc5396bfa3b56e17e45318c72a3ad0c5cb92
blacktop <blacktop@users.noreply.github.com>
2023-01-22 23:09:33 -0700

    feat: 🆕 `ipsw macho search` cmd

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -391,7 +391,7 @@ func Unzip(src, dest string, filter func(f *zip.File) bool) ([]string, error) {
 					panic(err)
 				}
 			}()
-			Indent(log.Info, 2)(fmt.Sprintf("Created %s", path))
+			Indent(log.Debug, 2)(fmt.Sprintf("Extracted %s", path))
 			_, err = io.Copy(f, rc)
 			if err != nil {
 				return err
