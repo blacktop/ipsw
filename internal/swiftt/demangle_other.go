ecf544925653a368426c090ffab9d15437e745e0
blacktop <blacktop@users.noreply.github.com>
2023-02-10 20:36:51 -0700

    Update demangle_other.go

--- internal/swiftt/demangle_other.go
+++ internal/swiftt/demangle_other.go
@@ -3,8 +3,8 @@
 package swift
 
 func Demangle(input string) (string, error) {
-	return "", nil // this is a no-op on non-darwin platforms
+	return input, nil // this is a no-op on non-darwin platforms
 }
 func DemangleSimple(input string) (string, error) {
-	return "", nil // this is a no-op on non-darwin platforms
+	return input, nil // this is a no-op on non-darwin platforms
 }
