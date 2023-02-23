e5b8037da7a665c443a7de109321f0522f79aaae
blacktop <blacktop@users.noreply.github.com>
2023-02-23 00:37:35 -0700

    feat: 🆕 `ipsw dyld ida` cmd to analyze the DSC in IDA Pro

---
+++ cmd/ipsw/cmd/dyld/dyld_ida.go
@@ -0,0 +1,186 @@
+/*
+Copyright © 2023 blacktop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package dyld
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/apex/log"
+	"github.com/blacktop/ipsw/internal/commands/ida"
+	"github.com/blacktop/ipsw/pkg/dyld"
+	"github.com/caarlos0/ctrlc"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	DyldCmd.AddCommand(idaCmd)
+
+	cwd, _ := os.Getwd()
+
+	idaCmd.Flags().StringP("ida-path", "p", "", "IDA Pro directory (darwin default: /Applications/IDA Pro */ida64.app/Contents/MacOS)")
+	idaCmd.Flags().BoolP("dependancies", "d", false, "Analyze module dependencies")
+	idaCmd.Flags().BoolP("enable-gui", "g", false, "Compress output folder")
+	idaCmd.Flags().BoolP("delete-db", "c", false, "Disassemble a new file (delete the old database)")
+	idaCmd.Flags().StringP("log-file", "l", "ida.log", "IDA log file")
+	idaCmd.Flags().StringP("output", "o", cwd, "Output folder")
+	idaCmd.Flags().String("slide", "", "dyld_shared_cache image ASLR slide value (hexadecimal)")
+	viper.BindPFlag("dyld.ida.ida-path", idaCmd.Flags().Lookup("ida-path"))
+	viper.BindPFlag("dyld.ida.dependancies", idaCmd.Flags().Lookup("dependancies"))
+	viper.BindPFlag("dyld.ida.enable-gui", idaCmd.Flags().Lookup("enable-gui"))
+	viper.BindPFlag("dyld.ida.delete-db", idaCmd.Flags().Lookup("delete-db"))
+	viper.BindPFlag("dyld.ida.log-file", idaCmd.Flags().Lookup("log-file"))
+	viper.BindPFlag("dyld.ida.output", idaCmd.Flags().Lookup("output"))
+	viper.BindPFlag("dyld.ida.slide", idaCmd.Flags().Lookup("slide"))
+}
+
+// idaCmd represents the ida command
+var idaCmd = &cobra.Command{
+	Use:           "ida <DSC> <DYLIB> [DYLIBS...]",
+	Short:         "Dump Swift Optimizations Info",
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	Args:          cobra.MinimumNArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if viper.GetBool("verbose") {
+			log.SetLevel(log.DebugLevel)
+		}
+
+		dscPath := filepath.Clean(args[0])
+
+		fileInfo, err := os.Lstat(dscPath)
+		if err != nil {
+			return fmt.Errorf("file %s does not exist", dscPath)
+		}
+
+		// Check if file is a symlink
+		if fileInfo.Mode()&os.ModeSymlink != 0 {
+			symlinkPath, err := os.Readlink(dscPath)
+			if err != nil {
+				return errors.Wrapf(err, "failed to read symlink %s", dscPath)
+			}
+			// TODO: this seems like it would break
+			linkParent := filepath.Dir(dscPath)
+			linkRoot := filepath.Dir(linkParent)
+
+			dscPath = filepath.Join(linkRoot, symlinkPath)
+		}
+
+		f, err := dyld.Open(dscPath)
+		if err != nil {
+			return fmt.Errorf("failed to open dyld shared cache %s: %w", dscPath, err)
+		}
+
+		var defaultframeworks = []string{
+			"/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation",
+			"/System/Library/Frameworks/Foundation.framework/Foundation",
+			"/usr/lib/libobjc.A.dylib",
+			"/usr/lib/system/libdyld.dylib",
+			"/usr/lib/system/libsystem_blocks.dylib",
+			"/usr/lib/system/libsystem_c.dylib",
+			"/usr/lib/system/libsystem_darwin.dylib",
+			"/usr/lib/system/libsystem_kernel.dylib",
+			"/usr/lib/system/libsystem_platform.dylib",
+			"/usr/lib/system/libsystem_trace.dylib",
+			"/usr/lib/system/libunwind.dylib",
+		}
+
+		if len(args) > 2 { // add any extra args to default frameworks
+			defaultframeworks = append(defaultframeworks, args[2:]...)
+		}
+
+		img, err := f.Image(args[1])
+		if err != nil {
+			return fmt.Errorf("failed to get image %s: %w", args[1], err)
+		}
+
+		if viper.GetBool("dyld.ida.dependancies") {
+			m, err := img.GetMacho()
+			if err != nil {
+				return fmt.Errorf("failed to get macho for %s: %w", img.Name, err)
+			}
+			defaultframeworks = append(defaultframeworks, m.ImportedLibraries()...)
+		}
+
+		dbFile := fmt.Sprintf("dsc_%s_%s_%s.i64", args[1], f.Headers[f.UUID].Platform, f.Headers[f.UUID].OsVersion)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		cli, err := ida.NewClient(ctx, &ida.Config{
+			IdaPath:    viper.GetString("dyld.ida.ida-path"),
+			InputFile:  dscPath,
+			Frameworks: defaultframeworks,
+			LogFile:    filepath.Join(viper.GetString("dyld.ida.output"), viper.GetString("dyld.ida.log-file")),
+			Output:     filepath.Join(viper.GetString("dyld.ida.output"), dbFile),
+			EnableGUI:  viper.GetBool("dyld.ida.enable-gui"),
+			// TempDatabase: viper.GetBool("temp-database"),
+			DeleteDB:   viper.GetBool("dyld.ida.delete-db"),
+			CompressDB: true,
+			FileType:   fmt.Sprintf("Apple DYLD cache for %s (single module)", strings.Split(f.Headers[f.UUID].Magic.String(), " ")[2]),
+			Env: []string{
+				fmt.Sprintf("IDA_DYLD_CACHE_MODULE=%s", img.Name),
+				fmt.Sprintf("IDA_DYLD_SHARED_CACHE_SLIDE=%s", viper.GetString("dyld.ida.slide")),
+			},
+			Options: []string{"objc:+l"},
+			// ScriptFile:   viper.GetString("script-file"),
+			// PluginFile:   viper.GetString("plugin-file"),
+			// DatabaseFile: viper.GetString("database-file"),
+			// Architecture: viper.GetString("architecture"),
+			// Platform:     viper.GetString("platform"),
+			// RemoteDebugger: ida.RemoteDebugger{
+			// 	Host: viper.GetString("remote-debugger-host"),
+			// 	Port: viper.GetInt("remote-debugger-port"),
+			// },
+		})
+		if err != nil {
+			return err
+		}
+
+		f.Close() // close the dyld_shared_cache file so IDA can open it
+
+		if err := ctrlc.Default.Run(ctx, func() error {
+			log.Info("Starting IDA Pro...")
+			if err := cli.Run(); err != nil {
+				return err
+			}
+			return nil
+		}); err != nil {
+			var cerr *ctrlc.ErrorCtrlC
+			if errors.As(err, &cerr) {
+				log.Warn("Exiting...")
+			} else {
+				return fmt.Errorf("failed to run IDA Pro: %v", err)
+			}
+		}
+
+		log.WithField("db", filepath.Join(viper.GetString("dyld.ida.output"), dbFile)).Info("🎉 Done!")
+
+		return nil
+	},
+}
---
+++ internal/commands/ida/ida.go
@@ -0,0 +1,265 @@
+package ida
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"html/template"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
+	"github.com/apex/log"
+	"github.com/blacktop/ipsw/internal/utils"
+	"github.com/pkg/errors"
+)
+
+const (
+	darwinPath  = "/Applications/IDA\\ Pro\\ */ida64.app/Contents/MacOS"
+	linuxPath   = ""
+	windowsPath = ""
+)
+
+const idaDscuPyScriptTemplate = `
+def dscu_load_module(module):
+	node = idaapi.netnode()
+	node.create("$ dscu")
+	node.supset(2, module)
+	load_and_run_plugin("dscu", 1)
+
+def dscu_load_region(ea):
+	node = idaapi.netnode()
+	node.create("$ dscu")
+	node.altset(3, ea)
+	load_and_run_plugin("dscu", 2)
+
+# load some commonly used system dylibs
+{{- range $framework := .Frameworks }}
+print("Loading {{ $framework }}")
+dscu_load_module("{{ $framework }}")
+{{- end }}
+
+print("analyzing objc types")
+load_and_run_plugin("objc", 1)
+print("analyzing NSConcreteGlobalBlock objects")
+load_and_run_plugin("objc", 4)
+
+# prevent IDA from creating functions with the noreturn attribute.
+# in dyldcache modules it is common that IDA will think a function doesn't return,
+# but in reality it just branches to an address outside of the current module.
+# this can break the analysis at times.
+idaapi.cvar.inf.af &= ~AF_ANORET
+
+print("perform autoanalysis...")
+auto_mark_range(0, BADADDR, AU_FINAL);
+auto_wait()
+
+print("analyzing NSConcreteStackBlock objects")
+load_and_run_plugin("objc", 5)
+
+# close IDA and save the database
+qexit(0)
+`
+
+type Config struct {
+	IdaPath    string
+	InputFile  string
+	Frameworks []string
+
+	AutoAnalyze  bool
+	AutoAccept   bool
+	BatchMode    bool
+	LogFile      string
+	Output       string
+	Env          []string
+	EntryPoint   string
+	EnableGUI    bool
+	TempDatabase bool
+	CompressDB   bool
+	DeleteDB     bool
+	Compiler     string
+	Processor    string
+	Options      []string
+	ScriptArgs   []string
+	PluginArgs   []string
+	FileType     string
+	ExtraArgs    []string
+}
+
+type Client struct {
+	conf *Config
+	cmd  *exec.Cmd
+}
+
+func NewClient(ctx context.Context, conf *Config) (*Client, error) {
+	var path string
+
+	cli := &Client{conf: conf}
+
+	if conf.IdaPath == "" {
+		switch runtime.GOOS {
+		case "darwin":
+			matches, err := filepath.Glob(darwinPath)
+			if err != nil {
+				return nil, err
+			}
+			if len(matches) == 0 {
+				return nil, fmt.Errorf("IDA Pro not found")
+			}
+			if len(matches) == 1 {
+				path = matches[0]
+			} else { // len(matches) > 1
+				prompt := &survey.Select{
+					Message: "Multiple IDA Pro versions found:",
+					Options: matches,
+				}
+				if err := survey.AskOne(prompt, &path); err != nil {
+					if err == terminal.InterruptErr {
+						log.Warn("Exiting...")
+						os.Exit(0)
+					}
+					return nil, err
+				}
+			}
+		case "linux":
+			// path = linuxPath
+			return nil, fmt.Errorf("supply IDA Pro path for linux (e.g. /opt/ida-7.0/)")
+		case "windows":
+			// path = windowsPath
+			return nil, fmt.Errorf("supply IDA Pro path for windows (e.g. C:\\Program Files\\IDA 7.0)")
+		default:
+			return nil, fmt.Errorf("unsupported OS: %s", runtime.GOOS)
+		}
+	} else {
+		path = conf.IdaPath
+	}
+
+	executable := filepath.Join(path, "idat64")
+	if conf.EnableGUI {
+		executable = filepath.Join(path, "ida64")
+	}
+
+	args := []string{}
+	if conf.AutoAnalyze {
+		args = append(args, "-a-")
+	} else {
+		args = append(args, "-a") // disable auto analysis. (-a- enables it)
+	}
+	if !conf.EnableGUI || conf.AutoAccept {
+		args = append(args, "-A") // autonomous mode. IDA will not display dialog boxes.
+		// Designed to be used together with -S switch.
+	}
+	if conf.BatchMode {
+		args = append(args, "-B") // batch mode. IDA will generate .IDB and .ASM files automatically
+	}
+	if conf.CompressDB {
+		args = append(args, "-P+") // compress database (create zipped idb)
+	} else {
+		args = append(args, "-P") // pack database (create unzipped idb)
+	}
+	if conf.LogFile != "" {
+		args = append(args, fmt.Sprintf("-L%s", conf.LogFile)) // name of the log file
+	}
+	if conf.DeleteDB {
+		args = append(args, "-c") // disassemble a new file (delete the old database)
+	}
+	if conf.Compiler != "" {
+		args = append(args, fmt.Sprintf("-C'%s'", conf.Compiler)) // set compiler in format name:abi
+	}
+	if conf.Processor != "" {
+		args = append(args, fmt.Sprintf("-p'%s'", conf.Processor)) // processor type
+	}
+	if conf.EntryPoint != "" {
+		args = append(args, fmt.Sprintf("-i%s", conf.EntryPoint)) // program entry point (hex)
+	}
+	if conf.TempDatabase {
+		args = append(args, "-DABANDON_DATABASE=YES")
+	}
+	if len(conf.Options) > 0 {
+		for _, opt := range conf.Options {
+			args = append(args, fmt.Sprintf("-O%s", opt))
+		}
+	}
+	if len(conf.ScriptArgs) > 0 {
+		// TODO: add script args
+	}
+	if len(conf.PluginArgs) > 0 {
+		// TODO: add plugin args (same thing as options)
+	}
+	// Script
+	script, err := cli.GenerateDSCUScript()
+	if err != nil {
+		return nil, err
+	}
+	tmp, err := os.CreateTemp("", "*.py")
+	if err != nil {
+		return nil, err
+	}
+	if _, err := tmp.WriteString(script); err != nil {
+		return nil, err
+	}
+	if err := tmp.Close(); err != nil {
+		return nil, err
+	}
+	/*
+		Execute a script file when the database is opened.
+		The script file extension is used to determine which extlang
+		will run the script.
+		It is possible to pass command line arguments after the script name.
+		For example: -S"myscript.idc argument1 \"argument 2\" argument3"
+		The passed parameters are stored in the "ARGV" global IDC variable.
+		Use "ARGV.count" to determine the number of arguments.
+		The first argument "ARGV[0]" contains the script name.
+	*/
+	args = append(args, fmt.Sprintf("-S%s", tmp.Name()))
+
+	if conf.FileType != "" {
+		/*
+			Interpret the input file as the specified file type
+			The file type is specified as a prefix of a file type
+			visible in the 'load file' dialog box
+			To specify archive member put it after the colon char,
+			for example: -TZIP:classes.dex
+			You can specify any nested paths:
+			-T<ftype>[:<member>{:<ftype>:<member>}[:<ftype>]]
+			IDA does not display the 'load file' dialog in this case
+		*/
+		args = append(args, fmt.Sprintf("-T'%s'", conf.FileType))
+	}
+	if conf.Output != "" {
+		args = append(args, fmt.Sprintf("-o%s", conf.Output)) // specify the output database (implies -c)
+	}
+	args = append(args, conf.ExtraArgs...)
+
+	args = append(args, conf.InputFile)
+
+	cli.cmd = exec.CommandContext(ctx, executable, args...)
+	cli.cmd.Env = append(os.Environ(), conf.Env...)
+	cli.cmd.Stdout = os.Stdout
+	cli.cmd.Stderr = os.Stderr
+
+	utils.Indent(log.Debug, 2)(cli.cmd.String())
+
+	return cli, nil
+}
+
+// Generate generates a IDAPython script from a template
+func (c *Client) GenerateDSCUScript() (string, error) {
+	var tplOut bytes.Buffer
+
+	tmpl := template.Must(template.New("ida").Funcs(template.FuncMap{"StringsJoin": strings.Join}).Parse(idaDscuPyScriptTemplate))
+
+	if err := tmpl.Execute(&tplOut, c.conf); err != nil {
+		return "", errors.Wrap(err, "failed to execute template")
+	}
+
+	return tplOut.String(), nil
+}
+
+func (c *Client) Run() error {
+	return c.cmd.Run()
+}
