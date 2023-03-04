b4cf850de0ff03259f7766971d5ab64a12b71aef
blacktop <blacktop@users.noreply.github.com>
2023-03-03 18:42:28 -0700

    fix: `ipsw kernel kmutil --filter` cmd to walk dependency graph to accurately remove bundles and bundles with a dependency path to them

--- pkg/kernelcache/kmutil.go
+++ pkg/kernelcache/kmutil.go
@@ -7,9 +7,10 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/apex/log"
 	"github.com/blacktop/go-macho"
 	"github.com/blacktop/go-plist"
+	"github.com/blacktop/ipsw/internal/utils"
+	"github.com/dominikbraun/graph"
 )
 
 func InspectKM(m *macho.File, filter string, explicitOnly, asJSON bool) (string, error) {
@@ -17,6 +18,7 @@ func InspectKM(m *macho.File, filter string, explicitOnly, asJSON bool) (string,
 	if err != nil {
 		return "", fmt.Errorf("failed to compile filter regex: %v", err)
 	}
+
 	if sec := m.Section("__PRELINK_INFO", "__info"); sec != nil {
 		dat, err := sec.Data()
 		if err != nil {
@@ -36,42 +38,62 @@ func InspectKM(m *macho.File, filter string, explicitOnly, asJSON bool) (string,
 			return string(jsonPL), nil
 		}
 
-		var out []string
+		var whitelist []string
+		var filterlist []string
+		var blacklist []string
 
 		for _, bundle := range prelink.PrelinkInfoDictionary {
 			if re.MatchString(bundle.ID) {
-				log.Debugf("found bundle '%s' who matches filter", bundle.ID)
+				filterlist = append(filterlist, bundle.ID)
+			}
+		}
+
+		g := graph.New(graph.StringHash, graph.Directed(), graph.PreventCycles())
+
+		// generate dependency graph
+		for _, bundle := range prelink.PrelinkInfoDictionary {
+			if len(bundle.OSBundleLibraries) > 0 {
+				g.AddVertex(bundle.ID)
+				for dependency := range bundle.OSBundleLibraries {
+					g.AddVertex(dependency)
+					if err := g.AddEdge(bundle.ID, dependency); err != nil {
+						return "", fmt.Errorf("failed to add edge: %v", err)
+					}
+				}
+			}
+		}
+
+		blacklist = append(blacklist, filterlist...)
+
+		// blacklist all bundles that dependacy paths contain filtered bundles
+		for _, skip := range filterlist {
+			for _, bundle := range prelink.PrelinkInfoDictionary {
+				path, err := graph.ShortestPath(g, bundle.ID, skip)
+				if err != nil {
+					continue
+				}
+				if len(path) > 0 {
+					blacklist = append(blacklist, bundle.ID)
+				}
+			}
+		}
+
+		// whitelist all bundles that are not blacklisted
+		for _, bundle := range prelink.PrelinkInfoDictionary {
+			if utils.StrSliceContains(blacklist, bundle.ID) {
 				continue
 			}
-			if len(bundle.OSBundleLibraries) == 0 {
-				if explicitOnly {
-					out = append(out, fmt.Sprintf("-b %s", bundle.ID))
-				} else {
-					out = append(out, bundle.ID)
-				}
+			if explicitOnly {
+				whitelist = append(whitelist, fmt.Sprintf("-b %s", bundle.ID))
 			} else {
-				skip := false
-				for name := range bundle.OSBundleLibraries {
-					if re.MatchString(name) {
-						log.Debugf("found bundle '%s' whose dependency matches filter", bundle.ID)
-						skip = true
-						break
-					}
-				}
-				if !skip {
-					if explicitOnly {
-						out = append(out, fmt.Sprintf("-b %s", bundle.ID))
-					} else {
-						out = append(out, bundle.ID)
-					}
-				}
+				whitelist = append(whitelist, bundle.ID)
 			}
 		}
 
 		if explicitOnly {
-			return strings.Join(out, " "), nil
+			return strings.Join(whitelist, " "), nil
 		}
-		return strings.Join(out, "\n"), nil
+		return strings.Join(whitelist, "\n"), nil
 	}
 
 	return "", fmt.Errorf("failed to find __PRELINK_INFO.__info section")
