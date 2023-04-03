b2514c60711b1b3620477b92b6a72131cd11be12
blacktop <blacktop@users.noreply.github.com>
2023-04-03 09:55:45 -0600

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,6 +2,7 @@
 package models
 
 import (
+	"os"
 	"time"
 
 	"gorm.io/gorm"
@@ -16,17 +17,29 @@ type IPSW struct {
 	Date       time.Time         `json:"date"`
 	Kernels    []Kernelcache     `json:"kernels"`
 	DSCs       []DyldSharedCache `json:"dscs"`
+	Files      FileSystem        `json:"files"`
 }
 
 // Kernelcache is the model for a kernelcache.
 type Kernelcache struct {
-	gorm.Model        // adds ID, created_at etc.
-	Version    string `json:"version"`
+	gorm.Model
+	Version string `json:"version"`
 }
 
 // DyldSharedCache is the model for a dyld_shared_cache.
 type DyldSharedCache struct {
-	gorm.Model        // adds ID, created_at etc.
-	Version    string `json:"version"`
-	Header     string `json:"header"`
+	gorm.Model
+	Version string `json:"version"`
+	Header  string `json:"header"`
+}
+
+type FileSystem struct {
+	gorm.Model
+	Files []File `json:"files"`
+}
+
+type File struct {
+	gorm.Model
+	Name string      `json:"name"`
+	Info os.FileInfo `json:"info"`
 }
