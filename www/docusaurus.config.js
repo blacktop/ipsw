53b7d55e7d5924bfca6fa8daab5e6b468c7fd503
blacktop <blacktop@users.noreply.github.com>
2023-04-18 14:14:00 -0600

    doc: add mermaid back in

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -27,7 +27,10 @@ const config = {
     defaultLocale: "en",
     locales: ["en"],
   },
-
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
   presets: [
     [
       "classic",
