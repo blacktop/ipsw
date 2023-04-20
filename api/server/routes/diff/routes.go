99b36d7c4bb0e92533d9d44206fe6029171e9c98
blacktop <blacktop@users.noreply.github.com>
2023-04-20 15:48:06 -0600

    doc: add `/diff` param doc

--- api/server/routes/diff/routes.go
+++ api/server/routes/diff/routes.go
@@ -12,6 +12,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// swagger:parameters postDiffFiles postDiffBlobs
+type diffFilesParams struct {
+	Previous string `json:"prev" binding:"required"`
+	Current  string `json:"curr" binding:"required"`
+}
+
 // swagger:response diffResponse
 type diffResponse struct {
 	Diff string `json:"diff"`
@@ -31,23 +37,22 @@ func AddRoutes(rg *gin.RouterGroup) {
 	//       400: genericError
 	//       500: genericError
 	dr.POST("/files", func(c *gin.Context) {
-		prevPath := c.Query("prev")
-		currPath := c.Query("curr")
-		if prevPath == "" || currPath == "" {
-			c.AbortWithStatusJSON(http.StatusBadRequest, types.GenericError{Error: "missing required 'prev' or 'curr' query parameter"})
+		var params diffFilesParams
+		if err := c.ShouldBindJSON(&params); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, types.GenericError{Error: err.Error()})
 			return
 		}
-		a, err := os.ReadFile(filepath.Clean(prevPath))
+		a, err := os.ReadFile(filepath.Clean(params.Previous))
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, types.GenericError{Error: err.Error()})
 			return
 		}
-		b, err := os.ReadFile(filepath.Clean(currPath))
+		b, err := os.ReadFile(filepath.Clean(params.Current))
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, types.GenericError{Error: err.Error()})
 			return
 		}
-		c.IndentedJSON(http.StatusOK, diffResponse{Diff: udiff.Unified(prevPath, currPath, fmt.Sprintln(a), fmt.Sprintln(b))})
+		c.IndentedJSON(http.StatusOK, diffResponse{Diff: udiff.Unified(params.Previous, params.Current, fmt.Sprintln(a), fmt.Sprintln(b))})
 	})
 	// swagger:route POST /diff/blobs Diff postDiffBlobs
 	//
@@ -59,12 +64,11 @@ func AddRoutes(rg *gin.RouterGroup) {
 	//       200: diffResponse
 	//       400: genericError
 	dr.POST("/blobs", func(c *gin.Context) {
-		prevBlob := c.Query("prev")
-		currBlob := c.Query("curr")
-		if prevBlob == "" || currBlob == "" {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "missing required 'prev' or 'curr' query parameter"})
+		var params diffFilesParams
+		if err := c.ShouldBindJSON(&params); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, types.GenericError{Error: err.Error()})
 			return
 		}
-		c.IndentedJSON(http.StatusOK, diffResponse{Diff: udiff.Unified("", "", fmt.Sprintln(prevBlob), fmt.Sprintln(currBlob))})
+		c.IndentedJSON(http.StatusOK, diffResponse{Diff: udiff.Unified("", "", fmt.Sprintln(params.Previous), fmt.Sprintln(params.Current))})
 	})
 }
