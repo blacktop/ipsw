efea434d41662dcf91bdbff6832fca88fb67057f
blacktop <blacktop@users.noreply.github.com>
2023-05-04 11:41:12 -0600

    feat: add 🆕 `ipsw download appledb` cmd which allows downloading betas w/o dev account

---
+++ cmd/ipsw/cmd/download/download_appledb.go
@@ -0,0 +1,242 @@
+/*
+Copyright © 2023 blacktop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package download
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/apex/log"
+	"github.com/blacktop/ipsw/internal/commands/extract"
+	"github.com/blacktop/ipsw/internal/download"
+	"github.com/blacktop/ipsw/internal/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var supportedOSes = []string{"audioOS", "bridgeOS", "iOS", "iPadOS", "iPodOS", "macOS", "tvOS", "watchOS"}
+
+func init() {
+	DownloadCmd.AddCommand(downloadAppledbCmd)
+
+	downloadAppledbCmd.Flags().String("os", "", "Operating system to download (i.e. iOS, tvOS, watchOS, bridgeOS)")
+	downloadAppledbCmd.Flags().Bool("kernel", false, "Extract kernelcache from remote IPSW")
+	downloadAppledbCmd.Flags().String("pattern", "", "Download remote files that match regex")
+	downloadAppledbCmd.Flags().Bool("beta", false, "Download beta IPSWs")
+	downloadAppledbCmd.Flags().StringP("output", "o", "", "Folder to download files to")
+	downloadAppledbCmd.Flags().BoolP("flat", "f", false, "Do NOT perserve directory structure when downloading with --pattern")
+
+	viper.BindPFlag("download.appledb.os", downloadAppledbCmd.Flags().Lookup("os"))
+	viper.BindPFlag("download.appledb.kernel", downloadAppledbCmd.Flags().Lookup("kernel"))
+	viper.BindPFlag("download.appledb.pattern", downloadAppledbCmd.Flags().Lookup("pattern"))
+	viper.BindPFlag("download.appledb.beta", downloadAppledbCmd.Flags().Lookup("beta"))
+	viper.BindPFlag("download.appledb.output", downloadAppledbCmd.Flags().Lookup("output"))
+	viper.BindPFlag("download.appledb.flat", downloadAppledbCmd.Flags().Lookup("flat"))
+
+	downloadAppledbCmd.SetHelpFunc(func(c *cobra.Command, s []string) {
+		DownloadCmd.PersistentFlags().MarkHidden("white-list")
+		DownloadCmd.PersistentFlags().MarkHidden("black-list")
+		DownloadCmd.PersistentFlags().MarkHidden("model") // TODO: remove this?
+		c.Parent().HelpFunc()(c, s)
+	})
+	downloadAppledbCmd.MarkFlagDirname("output")
+	downloadAppledbCmd.MarkFlagRequired("os")
+	downloadAppledbCmd.RegisterFlagCompletionFunc("os", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return supportedOSes, cobra.ShellCompDirectiveDefault
+	})
+}
+
+// downloadAppledbCmd represents the appledb command
+var downloadAppledbCmd = &cobra.Command{
+	Use:     "appledb",
+	Aliases: []string{"db"},
+	Short:   "Download IPSWs from appledb",
+	Example: `  # Download the iOS 16.5 beta 4 kernelcache from remote IPSW
+  ❯ ipsw download appledb --os iOS --version '16.4 beta 4' --device iPhone15,2 --kernel
+   • Parsing remote IPSW       build=20E5239b devices=iPhone15,2 version=16.4
+   • Extracting remote kernelcache
+      • Writing 20E5239b__iPhone15,2/kernelcache.release.iPhone15,2`,
+	Args:          cobra.NoArgs,
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if viper.GetBool("verbose") {
+			log.SetLevel(log.DebugLevel)
+		}
+		// parent flags
+		viper.BindPFlag("download.proxy", cmd.Flags().Lookup("proxy"))
+		viper.BindPFlag("download.insecure", cmd.Flags().Lookup("insecure"))
+		viper.BindPFlag("download.confirm", cmd.Flags().Lookup("confirm"))
+		viper.BindPFlag("download.skip-all", cmd.Flags().Lookup("skip-all"))
+		viper.BindPFlag("download.resume-all", cmd.Flags().Lookup("resume-all"))
+		viper.BindPFlag("download.restart-all", cmd.Flags().Lookup("restart-all"))
+		viper.BindPFlag("download.remove-commas", cmd.Flags().Lookup("remove-commas"))
+		viper.BindPFlag("download.device", cmd.Flags().Lookup("device"))
+		viper.BindPFlag("download.version", cmd.Flags().Lookup("version"))
+		viper.BindPFlag("download.build", cmd.Flags().Lookup("build"))
+		// settings
+		proxy := viper.GetString("download.proxy")
+		insecure := viper.GetBool("download.insecure")
+		confirm := viper.GetBool("download.confirm")
+		skipAll := viper.GetBool("download.skip-all")
+		resumeAll := viper.GetBool("download.resume-all")
+		restartAll := viper.GetBool("download.restart-all")
+		removeCommas := viper.GetBool("download.remove-commas")
+		// filters
+		device := viper.GetString("download.device")
+		version := viper.GetString("download.version")
+		build := viper.GetString("download.build")
+		// flags
+		osType := viper.GetString("download.appledb.os")
+		kernel := viper.GetBool("download.appledb.kernel")
+		pattern := viper.GetString("download.appledb.pattern")
+		isBeta := viper.GetBool("download.appledb.beta")
+		output := viper.GetString("download.appledb.output")
+		flat := viper.GetBool("download.appledb.flat")
+		// verify args
+		if !utils.StrSliceHas(supportedOSes, osType) {
+			return fmt.Errorf("valid --os flag choices are: %v", supportedOSes)
+		}
+
+		var destPath string
+		if len(output) > 0 {
+			destPath = filepath.Clean(output)
+		}
+
+		results, err := download.AppleDBQuery(&download.ADBQuery{
+			OS:       osType,
+			Version:  version,
+			Build:    build,
+			Device:   device,
+			IsBeta:   isBeta,
+			Proxy:    proxy,
+			Insecure: insecure,
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		log.Debug("URLs to download:")
+		for _, result := range results {
+			for _, link := range result.Links {
+				if link.Active && link.Preferred {
+					utils.Indent(log.Debug, 2)(link.URL)
+				}
+			}
+		}
+
+		cont := true
+		if !confirm {
+			if len(results) > 1 { // if filtered to a single device skip the prompt
+				cont = false
+				prompt := &survey.Confirm{
+					Message: fmt.Sprintf("You are about to download %d IPSW files. Continue?", len(results)),
+				}
+				survey.AskOne(prompt, &cont)
+			}
+		}
+
+		if cont {
+			if kernel || len(pattern) > 0 {
+				for _, result := range results {
+					var url string
+					for _, link := range result.Links {
+						if link.Active && link.Preferred {
+							url = link.URL
+						}
+					}
+					d, v, b := download.ParseIpswURLString(url)
+					log.WithFields(log.Fields{"devices": d, "build": b, "version": v}).Info("Parsing remote IPSW")
+
+					config := &extract.Config{
+						URL:      url,
+						Pattern:  pattern,
+						Proxy:    proxy,
+						Insecure: insecure,
+						Flatten:  flat,
+						Progress: true,
+						Output:   output,
+					}
+
+					cwd, err := os.Getwd()
+					if err != nil {
+						return fmt.Errorf("failed to get current working directory: %v", err)
+					}
+
+					// REMOTE KERNEL MODE
+					if kernel {
+						log.Info("Extracting remote kernelcache")
+						artifacts, err := extract.Kernelcache(config)
+						if err != nil {
+							return fmt.Errorf("failed to extract kernelcache from remote IPSW: %v", err)
+						}
+						for _, artifact := range artifacts {
+							utils.Indent(log.Info, 2)("Extracted " + strings.TrimPrefix(cwd, artifact))
+						}
+					}
+					// PATTERN MATCHING MODE
+					if len(pattern) > 0 {
+						log.Infof("Downloading files matching pattern %#v", pattern)
+						artifacts, err := extract.Search(config)
+						if err != nil {
+							return err
+						}
+						for _, artifact := range artifacts {
+							utils.Indent(log.Info, 2)("Extracted " + strings.TrimPrefix(cwd, artifact))
+						}
+					}
+				}
+			} else { // NORMAL MODE
+				downloader := download.NewDownload(proxy, insecure, skipAll, resumeAll, restartAll, false, viper.GetBool("verbose"))
+				for idx, result := range results {
+					var url string
+					for _, link := range result.Links {
+						if link.Active && link.Preferred {
+							url = link.URL
+						}
+					}
+					fname := filepath.Join(destPath, getDestName(url, removeCommas))
+					if _, err := os.Stat(fname); os.IsNotExist(err) {
+						d, v, b := download.ParseIpswURLString(url)
+						log.WithFields(log.Fields{"devices": d, "build": b, "version": v}).Infof("Getting (%d/%d) IPSW", idx+1, len(results))
+						// download file
+						downloader.URL = url
+						downloader.DestName = fname
+
+						err = downloader.Do()
+						if err != nil {
+							return fmt.Errorf("failed to download IPSW: %v", err)
+						}
+					} else {
+						log.Warnf("IPSW already exists: %s", fname)
+					}
+				}
+			}
+		}
+
+		return nil
+	},
+}
---
+++ internal/download/appledb.go
@@ -0,0 +1,232 @@
+package download
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/blacktop/ipsw/internal/utils"
+)
+
+const (
+	// AppleDBRepoURL is the URL to the AppleDB github repo
+	AppleDBRepoURL = "https://github.com/littlebyteorg/appledb"
+	ApiContentsURL = "https://api.github.com/repos/littlebyteorg/appledb/contents/"
+)
+
+// GithubContentsResponse is the response from the GET /repos/{owner}/{repo}/contents/{path} github api
+type GithubContentsResponse struct {
+	Type        string `json:"type"`
+	Encoding    string `json:"encoding"`
+	Size        int    `json:"size"`
+	Name        string `json:"name"`
+	Path        string `json:"path"`
+	Content     string `json:"content"`
+	Sha         string `json:"sha"`
+	URL         string `json:"url"`
+	GitURL      string `json:"git_url"`
+	HTMLURL     string `json:"html_url"`
+	DownloadURL string `json:"download_url"`
+	Links       struct {
+		Git  string `json:"git"`
+		Self string `json:"self"`
+		HTML string `json:"html"`
+	} `json:"_links"`
+}
+
+type OsFileSource struct {
+	Type      string   `json:"type"`
+	DeviceMap []string `json:"deviceMap"`
+	Links     []struct {
+		URL       string `json:"url"`
+		Preferred bool   `json:"preferred"`
+		Active    bool   `json:"active"`
+	} `json:"links"`
+	Hashes struct {
+		Sha2256 string `json:"sha2-256"`
+		Sha1    string `json:"sha1"`
+	} `json:"hashes"`
+	Size int64 `json:"size"`
+}
+
+// AppleDbOsFiles is an AppleDB osFiles object
+type AppleDbOsFile struct {
+	OS        string         `json:"osStr"`
+	Version   string         `json:"version"`
+	Build     string         `json:"build"`
+	Released  string         `json:"released"`
+	Beta      bool           `json:"beta"`
+	DeviceMap []string       `json:"deviceMap"`
+	Sources   []OsFileSource `json:"sources"`
+}
+
+type OsFiles []AppleDbOsFile
+
+type ADBQuery struct {
+	OS       string
+	Version  string
+	Build    string
+	Device   string
+	IsBeta   bool
+	Proxy    string
+	Insecure bool
+}
+
+func (fs OsFiles) Query(query *ADBQuery) []OsFileSource {
+	var sources []OsFileSource
+	for _, f := range fs {
+		if len(query.OS) > 0 && f.OS != query.OS {
+			continue
+		}
+		if len(query.Version) > 0 && f.Version != query.Version {
+			continue
+		}
+		if len(query.Build) > 0 && f.Build != query.Build {
+			continue
+		}
+		if query.IsBeta && !f.Beta {
+			continue
+		}
+		if len(query.Device) > 0 {
+			for _, source := range f.Sources {
+				if utils.StrSliceContains(source.DeviceMap, query.Device) {
+					sources = append(sources, source)
+				}
+			}
+		} else {
+			sources = append(sources, f.Sources...)
+		}
+	}
+	return sources
+}
+
+func AppleDBQuery(q *ADBQuery) ([]OsFileSource, error) {
+	var osfiles OsFiles
+
+	qurl, err := url.JoinPath("osFiles", q.OS)
+	if err != nil {
+		return nil, err
+	}
+
+	folders, err := queryGithubAPI(qurl, q.Proxy, q.Insecure)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, folder := range folders {
+		build, version, found := strings.Cut(folder.Name, " - ")
+		if !found {
+			continue
+		}
+		if len(q.Version) > 0 && !strings.HasPrefix(q.Version, strings.TrimSuffix(version, "x")) {
+			continue
+		}
+		if len(q.Build) > 0 && !strings.HasPrefix(q.Build, strings.TrimSuffix(build, "x")) {
+			continue
+		}
+
+		qurl, err = url.JoinPath("osFiles", q.OS, folder.Name)
+		if err != nil {
+			return nil, err
+		}
+		files, err := queryGithubAPI(qurl, q.Proxy, q.Insecure)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, file := range files {
+			of, err := getOsFiles(file.DownloadURL, q.Proxy, q.Insecure)
+			if err != nil {
+				return nil, err
+			}
+			osfiles = append(osfiles, *of)
+		}
+	}
+
+	return osfiles.Query(q), nil
+}
+
+func queryGithubAPI(path, proxy string, insecure bool) ([]GithubContentsResponse, error) {
+	var contents []GithubContentsResponse
+
+	req, err := http.NewRequest("GET", ApiContentsURL+path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create http GET request: %v", err)
+	}
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+	req.Header.Add("User-Agent", utils.RandomAgent())
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy:           GetProxy(proxy),
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
+		},
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("api returned status: %s", res.Status)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	res.Body.Close()
+
+	if err := json.Unmarshal(body, &contents); err != nil {
+		return nil, err
+	}
+
+	return contents, nil
+}
+
+func getOsFiles(path, proxy string, insecure bool) (*AppleDbOsFile, error) {
+	var osfile AppleDbOsFile
+
+	req, err := http.NewRequest("GET", path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create http GET request: %v", err)
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Add("User-Agent", utils.RandomAgent())
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy:           GetProxy(proxy),
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
+		},
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("returned status: %s", res.Status)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	res.Body.Close()
+
+	if err := json.Unmarshal(body, &osfile); err != nil {
+		return nil, err
+	}
+
+	return &osfile, nil
+}
