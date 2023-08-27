a4369ea1db71eb126b84e670d07c10701a719e64
blacktop <blacktop@users.noreply.github.com>
2023-08-26 21:42:38 -0600

    chore: working on `ipsw appstore bundle cap add` command to add capabilities to bundleIDs

---
+++ cmd/ipsw/cmd/appstore/appstore_bundle_cap_add.go
@@ -0,0 +1,110 @@
+/*
+Copyright © 2018-2023 blacktop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package appstore
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
+	"github.com/apex/log"
+	"github.com/blacktop/ipsw/pkg/appstore"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	ASBundleCapabilityCmd.AddCommand(ASBundleCapabilityAddCmd)
+
+	ASBundleCapabilityAddCmd.Flags().String("id", "", "Profile ID to renew")
+	viper.BindPFlag("appstore.bundle.cap.add.id", ASBundleCapabilityAddCmd.Flags().Lookup("id"))
+}
+
+// ASBundleCapabilityAddCmd represents the appstore cert ls command
+var ASBundleCapabilityAddCmd = &cobra.Command{
+	Use:           "add",
+	Short:         "🚧 Enable a capability for a bundle ID",
+	Args:          cobra.NoArgs,
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	Hidden:        true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if viper.GetBool("verbose") {
+			log.SetLevel(log.DebugLevel)
+		}
+
+		// parent flags
+		viper.BindPFlag("appstore.p8", cmd.Flags().Lookup("p8"))
+		viper.BindPFlag("appstore.iss", cmd.Flags().Lookup("iss"))
+		viper.BindPFlag("appstore.kid", cmd.Flags().Lookup("kid"))
+		// flags
+		bid := viper.GetString("appstore.bundle.cap.add.id")
+		// Validate flags
+		if viper.GetString("appstore.p8") == "" || viper.GetString("appstore.iss") == "" || viper.GetString("appstore.kid") == "" {
+			return fmt.Errorf("you must provide --p8, --iss and --kid")
+		}
+
+		as := appstore.NewAppStore(viper.GetString("appstore.p8"), viper.GetString("appstore.iss"), viper.GetString("appstore.kid"))
+
+		if len(bid) == 0 { // Pick Board ID
+			bids, err := as.GetBundleIDs()
+			if err != nil {
+				return err
+			}
+
+			var choices []string
+			for _, b := range bids {
+				choices = append(choices, fmt.Sprintf("%s (%s)", b.Attributes.ID, b.Attributes.Name))
+			}
+
+			var choice string
+			prompt := &survey.Select{
+				Message:  "Select buildID to use:",
+				Options:  choices,
+				PageSize: 10,
+			}
+			if err := survey.AskOne(prompt, &choice); err == terminal.InterruptErr {
+				log.Warn("Exiting...")
+				return nil
+			}
+
+			for _, b := range bids {
+				if strings.HasPrefix(choice, b.Attributes.ID+" (") {
+					bid = b.ID
+					break
+				}
+			}
+		}
+
+		caps, err := as.EnableCapability(bid)
+		if err != nil {
+			return err
+		}
+
+		log.Info("Added Capability:")
+		log.Infof("%s: %s", caps.ID, caps.Attributes.CapabilityType)
+
+		return nil
+	},
+}
---
+++ pkg/appstore/cap.go
@@ -0,0 +1,186 @@
+package appstore
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/blacktop/ipsw/internal/download"
+)
+
+type capabilityType string
+
+const (
+	ICLOUD                           capabilityType = "ICLOUD"
+	IN_APP_PURCHASE                  capabilityType = "IN_APP_PURCHASE"
+	GAME_CENTER                      capabilityType = "GAME_CENTER"
+	PUSH_NOTIFICATIONS               capabilityType = "PUSH_NOTIFICATIONS"
+	WALLET                           capabilityType = "WALLET"
+	INTER_APP_AUDIO                  capabilityType = "INTER_APP_AUDIO"
+	MAPS                             capabilityType = "MAPS"
+	ASSOCIATED_DOMAINS               capabilityType = "ASSOCIATED_DOMAINS"
+	PERSONAL_VPN                     capabilityType = "PERSONAL_VPN"
+	APP_GROUPS                       capabilityType = "APP_GROUPS"
+	HEALTHKIT                        capabilityType = "HEALTHKIT"
+	HOMEKIT                          capabilityType = "HOMEKIT"
+	WIRELESS_ACCESSORY_CONFIGURATION capabilityType = "WIRELESS_ACCESSORY_CONFIGURATION"
+	APPLE_PAY                        capabilityType = "APPLE_PAY"
+	DATA_PROTECTION                  capabilityType = "DATA_PROTECTION"
+	SIRIKIT                          capabilityType = "SIRIKIT"
+	NETWORK_EXTENSIONS               capabilityType = "NETWORK_EXTENSIONS"
+	MULTIPATH                        capabilityType = "MULTIPATH"
+	HOT_SPOT                         capabilityType = "HOT_SPOT"
+	NFC_TAG_READING                  capabilityType = "NFC_TAG_READING"
+	CLASSKIT                         capabilityType = "CLASSKIT"
+	AUTOFILL_CREDENTIAL_PROVIDER     capabilityType = "AUTOFILL_CREDENTIAL_PROVIDER"
+	ACCESS_WIFI_INFORMATION          capabilityType = "ACCESS_WIFI_INFORMATION"
+	NETWORK_CUSTOM_PROTOCOL          capabilityType = "NETWORK_CUSTOM_PROTOCOL"
+	COREMEDIA_HLS_LOW_LATENCY        capabilityType = "COREMEDIA_HLS_LOW_LATENCY"
+	SYSTEM_EXTENSION_INSTALL         capabilityType = "SYSTEM_EXTENSION_INSTALL"
+	USER_MANAGEMENT                  capabilityType = "USER_MANAGEMENT"
+	APPLE_ID_AUTH                    capabilityType = "APPLE_ID_AUTH"
+)
+
+type allowedInstanceType string
+
+const (
+	ENTRY    allowedInstanceType = "ENTRY"
+	SINGLE   allowedInstanceType = "SINGLE"
+	MULTIPLE allowedInstanceType = "MULTIPLE"
+)
+
+type capabilityKey string
+
+const (
+	ICLOUD_VERSION                   capabilityKey = "ICLOUD_VERSION"
+	DATA_PROTECTION_PERMISSION_LEVEL capabilityKey = "DATA_PROTECTION_PERMISSION_LEVEL"
+	APPLE_ID_AUTH_APP_CONSENT        capabilityKey = "APPLE_ID_AUTH_APP_CONSENT"
+)
+
+type capabilityOptionKey string
+
+const (
+	XCODE_5                         capabilityOptionKey = "XCODE_5"
+	XCODE_6                         capabilityOptionKey = "XCODE_6"
+	COMPLETE_PROTECTION             capabilityOptionKey = "COMPLETE_PROTECTION"
+	PROTECTED_UNLESS_OPEN           capabilityOptionKey = "PROTECTED_UNLESS_OPEN"
+	PROTECTED_UNTIL_FIRST_USER_AUTH capabilityOptionKey = "PROTECTED_UNTIL_FIRST_USER_AUTH"
+	PRIMARY_APP_CONSENT             capabilityOptionKey = "PRIMARY_APP_CONSENT"
+)
+
+type CapabilityOption struct {
+	Description      string              `json:"description"`
+	Enabled          bool                `json:"enabled"`
+	Default          bool                `json:"enabledByDefault"`
+	Key              capabilityOptionKey `json:"key"`
+	Name             string              `json:"name"`
+	SupportsWildcard bool                `json:"supportsWildcard"`
+}
+
+type CapabilitySetting struct {
+	AllowedInstances allowedInstanceType `json:"allowedInstances"`
+	Description      string              `json:"description"`
+	Enabled          bool                `json:"enabledByDefault"`
+	Key              capabilityKey       `json:"key"`
+	Name             string              `json:"name"`
+	Options          []CapabilityOption  `json:"options"`
+	Visible          bool                `json:"visible"`
+	MinInstances     int                 `json:"minInstances"`
+}
+
+type BundleIdCapability struct {
+	ID         string `json:"id"`
+	Type       string `json:"type"` // bundleIdCapabilities
+	Attributes struct {
+		CapabilityType capabilityType      `json:"capabilityType"`
+		Settings       []CapabilitySetting `json:"settings"`
+	} `json:"attributes"`
+	Links ResourceLinks `json:"links"`
+}
+
+type BundleIdCapabilitiesResponse struct {
+	Data  []BundleIdCapability `json:"data"`
+	Links Links                `json:"links"`
+	Meta  Meta                 `json:"meta"`
+}
+
+type BundleIdCapabilityResponse struct {
+	Data  BundleIdCapability `json:"data"`
+	Links DocumentLinks      `json:"links"`
+}
+
+type BundleIdCapabilityCreateRequest struct {
+	Data struct {
+		Type       string `json:"type"` // bundleIdCapabilities
+		Attributes struct {
+			CapabilityType capabilityType      `json:"capabilityType"`
+			Settings       []CapabilitySetting `json:"settings"`
+		} `json:"attributes"`
+		Relationships struct {
+			BundleID struct {
+				Data struct {
+					ID   string `json:"id"`
+					Type string `json:"type"` // bundleIds
+				} `json:"data"`
+			} `json:"bundleId"`
+		} `json:"relationships"`
+	} `json:"data"`
+}
+
+// EnableCapability enables a capability for a bundle ID.. // TODO: finish this
+func (as *AppStore) EnableCapability(id string) (*BundleIdCapability, error) {
+	if err := as.createToken(); err != nil {
+		return nil, fmt.Errorf("failed to create token: %v", err)
+	}
+
+	var bundleIdCapabilityCreateRequest BundleIdCapabilityCreateRequest
+	bundleIdCapabilityCreateRequest.Data.Type = "bundleIdCapabilities"
+
+	jsonStr, err := json.Marshal(&bundleIdCapabilityCreateRequest)
+	if err != nil {
+		return nil, err
+	}
+	// os.WriteFile("req.json", jsonStr, 0644)
+	req, err := http.NewRequest("POST", bundleIDCapabilitiesURL, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+as.token)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy:           download.GetProxy(as.Proxy),
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: as.Insecure},
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send http request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 201 {
+		var eresp ErrorResponse
+		if err := json.NewDecoder(resp.Body).Decode(&eresp); err != nil {
+			return nil, fmt.Errorf("failed to JSON decode http response: %v", err)
+		}
+		var errOut string
+		for idx, e := range eresp.Errors {
+			errOut += fmt.Sprintf("%s%s: %s (%s)\n", strings.Repeat("\t", idx), e.Code, e.Title, e.Detail)
+		}
+		return nil, fmt.Errorf("%s: %s", resp.Status, errOut)
+	}
+
+	var bcap BundleIdCapabilityResponse
+	if err := json.NewDecoder(resp.Body).Decode(&bcap); err != nil {
+		return nil, fmt.Errorf("failed to JSON decode http response: %v", err)
+	}
+
+	return &bcap.Data, nil
+}
