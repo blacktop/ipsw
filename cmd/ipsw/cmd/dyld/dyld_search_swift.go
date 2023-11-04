1d3d48569f4ed5f031c756e3b04435e2d0f3cc4d
blacktop <blacktop@users.noreply.github.com>
2023-11-04 11:37:41 -0600

    feat: add beginnings of a NEW `ipsw dsc search swift` command to search for Swift metadata in DSC

---
+++ cmd/ipsw/cmd/dyld/dyld_search_swift.go
@@ -0,0 +1,125 @@
+/*
+Copyright © 2023 blacktop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package dyld
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/apex/log"
+	"github.com/blacktop/go-macho"
+	"github.com/blacktop/go-macho/types/swift"
+	"github.com/blacktop/ipsw/pkg/dyld"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	dyldSearchCmd.AddCommand(dyldSearchSwiftCmd)
+
+	dyldSearchSwiftCmd.Flags().StringP("class", "c", "", "Search for specific Swift class regex")
+
+	viper.BindPFlag("dyld.search.swift.class", dyldSearchSwiftCmd.Flags().Lookup("class"))
+
+	// dyldSearchSwiftCmd.MarkFlagsMutuallyExclusive("class", "")
+}
+
+// dyldSearchSwiftCmd represents the swift command
+var dyldSearchSwiftCmd = &cobra.Command{
+	Use:           "swift",
+	Aliases:       []string{"s"},
+	Short:         "Find Dylib files for given Swift search criteria",
+	Args:          cobra.ExactArgs(1),
+	SilenceErrors: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if viper.GetBool("verbose") {
+			log.SetLevel(log.DebugLevel)
+		}
+
+		// flags
+		classReStr := viper.GetString("dyld.search.swift.class")
+		// verify flags
+		if classReStr == "" {
+			return fmt.Errorf("must specify a search criteria via one of the flags")
+		}
+
+		dscPath := filepath.Clean(args[0])
+
+		fileInfo, err := os.Lstat(dscPath)
+		if err != nil {
+			return fmt.Errorf("file %s does not exist", dscPath)
+		}
+
+		// Check if file is a symlink
+		if fileInfo.Mode()&os.ModeSymlink != 0 {
+			symlinkPath, err := os.Readlink(dscPath)
+			if err != nil {
+				return fmt.Errorf("failed to read symlink %s: %v", dscPath, err)
+			}
+			// TODO: this seems like it would break
+			linkParent := filepath.Dir(dscPath)
+			linkRoot := filepath.Dir(linkParent)
+
+			dscPath = filepath.Join(linkRoot, symlinkPath)
+		}
+
+		f, err := dyld.Open(dscPath)
+		if err != nil {
+			return fmt.Errorf("failed to open dyld shared cache %s: %w", dscPath, err)
+		}
+
+		for _, image := range f.Images {
+			m, err := image.GetMacho()
+			if err != nil {
+				return err
+			}
+			if m.HasSwift() {
+				if classReStr != "" {
+					classRE, err := regexp.Compile(classReStr)
+					if err != nil {
+						return fmt.Errorf("invalid regex '%s': %w", classReStr, err)
+					}
+					if err := m.PreCache(); err != nil { // cache fields and types
+						log.Errorf("failed to precache swift fields/types for %s: %v", filepath.Base(image.Name), err)
+					}
+					if typs, err := m.GetSwiftTypes(); err == nil {
+						for _, typ := range typs {
+							if typ.Kind == swift.CDKindClass {
+								if classRE.MatchString(typ.Name) {
+									fmt.Printf("%s: %s\t%s=%s\n", colorAddr("%#09x", typ.Address), colorImage(filepath.Base(image.Name)), colorField("class"), typ.Name)
+								}
+							}
+						}
+					} else if !errors.Is(err, macho.ErrSwiftSectionError) {
+						log.Errorf("failed to parse swift types for %s: %v", filepath.Base(image.Name), err)
+					}
+				}
+			}
+		}
+
+		return nil
+	},
+}
