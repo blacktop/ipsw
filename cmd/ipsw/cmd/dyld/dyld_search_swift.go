4408b1b75b9b62a14adeb7257d654331db2d2ee5
blacktop <blacktop@users.noreply.github.com>
2023-11-05 10:54:59 -0700

    feat: add `--protocol` flag to `ipsw dsc search swift` command

--- cmd/ipsw/cmd/dyld/dyld_search_swift.go
+++ cmd/ipsw/cmd/dyld/dyld_search_swift.go
@@ -39,9 +39,12 @@ import (
 func init() {
 	dyldSearchCmd.AddCommand(dyldSearchSwiftCmd)
 
+	dyldSearchSwiftCmd.Flags().StringSliceP("image", "i", []string{}, "Images to search (default: all)")
 	dyldSearchSwiftCmd.Flags().StringP("class", "c", "", "Search for specific Swift class regex")
-
+	dyldSearchSwiftCmd.Flags().StringP("protocol", "p", "", "Search for specific Swift protocol regex")
+	viper.BindPFlag("dyld.search.swift.image", dyldSearchSwiftCmd.Flags().Lookup("image"))
 	viper.BindPFlag("dyld.search.swift.class", dyldSearchSwiftCmd.Flags().Lookup("class"))
+	viper.BindPFlag("dyld.search.swift.protocol", dyldSearchSwiftCmd.Flags().Lookup("protocol"))
 
 	// dyldSearchSwiftCmd.MarkFlagsMutuallyExclusive("class", "")
 }
@@ -53,17 +56,38 @@ var dyldSearchSwiftCmd = &cobra.Command{
 	Short:         "Find Dylib files for given Swift search criteria",
 	Args:          cobra.ExactArgs(1),
 	SilenceErrors: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
 
 		if viper.GetBool("verbose") {
 			log.SetLevel(log.DebugLevel)
 		}
 
 		// flags
+		searchImages := viper.GetStringSlice("dyld.search.swift.image")
 		classReStr := viper.GetString("dyld.search.swift.class")
+		protocolReStr := viper.GetString("dyld.search.swift.protocol")
 		// verify flags
-		if classReStr == "" {
+		if classReStr == "" && protocolReStr == "" {
 			return fmt.Errorf("must specify a search criteria via one of the flags")
+		}
+		// compile regexes
+		var classRE *regexp.Regexp
+		if classReStr != "" {
+			if classReStr != "" {
+				classRE, err = regexp.Compile(classReStr)
+				if err != nil {
+					return fmt.Errorf("invalid regex '%s': %w", classReStr, err)
+				}
+			}
+		}
+		var protocolRE *regexp.Regexp
+		if protocolReStr != "" {
+			if protocolReStr != "" {
+				protocolRE, err = regexp.Compile(protocolReStr)
+				if err != nil {
+					return fmt.Errorf("invalid regex '%s': %w", protocolReStr, err)
+				}
+			}
 		}
 
 		dscPath := filepath.Clean(args[0])
@@ -91,30 +115,71 @@ var dyldSearchSwiftCmd = &cobra.Command{
 			return fmt.Errorf("failed to open dyld shared cache %s: %w", dscPath, err)
 		}
 
-		for _, image := range f.Images {
+		var images []*dyld.CacheImage
+		if len(searchImages) == 0 {
+			images = f.Images
+		} else {
+			for _, img := range searchImages {
+				image, err := f.Image(img)
+				if err != nil {
+					return fmt.Errorf("failed to find image %s in %s: %v", img, dscPath, err)
+				}
+				images = append(images, image)
+			}
+		}
+
+		for _, image := range images {
 			m, err := image.GetMacho()
 			if err != nil {
 				return err
 			}
 			if m.HasSwift() {
-				if classReStr != "" {
-					classRE, err := regexp.Compile(classReStr)
-					if err != nil {
-						return fmt.Errorf("invalid regex '%s': %w", classReStr, err)
-					}
-					if err := m.PreCache(); err != nil { // cache fields and types
-						log.Errorf("failed to precache swift fields/types for %s: %v", filepath.Base(image.Name), err)
-					}
+				if err := m.PreCache(); err != nil { // cache fields and types
+					log.Errorf("failed to precache swift fields/types for %s: %v", filepath.Base(image.Name), err)
+				}
+				if classRE != nil || protocolRE != nil {
 					if typs, err := m.GetSwiftTypes(); err == nil {
 						for _, typ := range typs {
-							if typ.Kind == swift.CDKindClass {
-								if classRE.MatchString(typ.Name) {
+							switch typ.Kind {
+							case swift.CDKindClass:
+								if classRE != nil && classRE.MatchString(typ.Name) {
 									fmt.Printf("%s: %s\t%s=%s\n", colorAddr("%#09x", typ.Address), colorImage(filepath.Base(image.Name)), colorField("class"), typ.Name)
+								}
+							case swift.CDKindProtocol:
+								if protocolRE != nil && protocolRE.MatchString(typ.Name) {
+									fmt.Printf("%s: %s\t%s=%s\n", colorAddr("%#09x", typ.Address), colorImage(filepath.Base(image.Name)), colorField("protocol"), typ.Name)
 								}
 							}
 						}
 					} else if !errors.Is(err, macho.ErrSwiftSectionError) {
 						log.Errorf("failed to parse swift types for %s: %v", filepath.Base(image.Name), err)
+					}
+				}
+				if protocolRE != nil {
+					if prots, err := m.GetSwiftProtocols(); err == nil {
+						for _, typ := range prots {
+							if protocolRE.MatchString(typ.Name) {
+								fmt.Printf("%s: %s\t%s=%s\n", colorAddr("%#09x", typ.Address), colorImage(filepath.Base(image.Name)), colorField("protocol"), typ.Name)
+							}
+						}
+					} else if !errors.Is(err, macho.ErrSwiftSectionError) {
+						log.Errorf("failed to parse swift protocols for %s: %v", filepath.Base(image.Name), err)
+					}
+					if confs, err := m.GetSwiftProtocolConformances(); err == nil {
+						for _, conf := range confs {
+							if protocolRE.MatchString(conf.Protocol) {
+								typName := conf.TypeRef.Name
+								if conf.TypeRef.Parent != nil && conf.TypeRef.Parent.Name != "" {
+									typName = conf.TypeRef.Parent.Name + "." + typName
+									if conf.TypeRef.Parent.Parent != nil && conf.TypeRef.Parent.Parent.Name != "" {
+										typName = conf.TypeRef.Parent.Parent.Name + "." + typName
+									}
+								}
+								fmt.Printf("%s: %s\t%s=%s\t%s=%s\n", colorAddr("%#09x", conf.Address), colorImage(filepath.Base(image.Name)), colorField("conformance"), conf.Protocol, colorField("type"), typName)
+							}
+						}
+					} else if !errors.Is(err, macho.ErrSwiftSectionError) {
+						log.Errorf("failed to parse swift protocols for %s: %v", filepath.Base(image.Name), err)
 					}
 				}
 			}
