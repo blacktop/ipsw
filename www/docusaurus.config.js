45dc03dd6b8d3566c06f8bdea0f998b51c7ccf74
blacktop <blacktop@users.noreply.github.com>
2023-11-04 19:56:12 -0600

    docs: fixes

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -1,9 +1,13 @@
-// @ts-nocheck
-// Note: type annotations allow type checking and IDEs autocompletion
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
 const path = require("path");
-const lightCodeTheme = require("prism-react-renderer/themes/vsDark");
-const darkCodeTheme = require("prism-react-renderer/themes/palenight");
+import {themes as prismThemes} from "prism-react-renderer";
+const lightCodeTheme = prismThemes.vsDark;
+const darkCodeTheme = prismThemes.palenight;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -19,9 +23,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "blacktop", // Usually your GitHub org/user name.
   projectName: "ipsw", // Usually your repo name.
-
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
