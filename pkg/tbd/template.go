22d2dee8bd7367637057c0a0adc3f5de5b0ca3e7
blacktop <blacktop@users.noreply.github.com>
2023-12-11 00:09:26 -0700

    feat: add support for reexports in `ipsw dsc tbd` command

--- pkg/tbd/template.go
+++ pkg/tbd/template.go
@@ -1,18 +1,20 @@
 package tbd
 
 const tbdTemplate = `--- !tapi-tbd
-tbd-version:     4
-targets:         [ {{ StringsJoin .Targets ", " }} ]
-install-name:    {{.Path}}
+tbd-version:          4
+targets:              [ {{ StringsJoin .Targets ", " }} ]
+install-name:         '{{.Path}}'
+{{ if .ReExports }}reexported-libraries:
+  - targets:          [ {{ StringsJoin .Targets ", " }} ]
+    libraries:        [ '{{ StringsJoin .ReExports "', '" }}' ]
+{{ end -}}
 exports:
-  - targets:         [ {{ StringsJoin .Targets ", " }} ]
-    symbols:         [ {{ StringsJoin .Symbols ",\n                       " }} ]
+  - targets:          [ {{ StringsJoin .Targets ", " }} ]
+    symbols:          [ {{ StringsJoin .Symbols ",\n                       " }} ]
 {{- if .ObjcClasses }}    
     objc-classes:    [ {{ StringsJoin .ObjcClasses ",\n                       " }} ]
 {{- end }}
 {{- if .ObjcIvars }}    
     objc-ivars:      [ {{ StringsJoin .ObjcIvars ",\n                       " }} ]
 {{- end }}
-...
-
 `
