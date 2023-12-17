824bd7fd3a05f6343c5f8eaf31ac73f0f749cc5a
blacktop <blacktop@users.noreply.github.com>
2023-12-17 15:19:12 -0700

    feat: add IPSW macho diffs to `ipsw diff` command #371

--- internal/magic/magic.go
+++ internal/magic/magic.go
@@ -35,6 +35,26 @@ func IsMachO(filePath string) (bool, error) {
 	case Magic32, Magic64, MagicFatBE, MagicFatLE:
 		return true, nil
 	default:
+		return false, fmt.Errorf("not a macho file")
+	}
+}
+
+func IsMachoOrImg4(filePath string) (bool, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return false, fmt.Errorf("failed to open file %s: %w", filePath, err)
+	}
+	defer f.Close()
+
+	var magic [4]byte
+	if _, err = f.Read(magic[:]); err != nil {
+		return false, fmt.Errorf("failed to read magic: %w", err)
+	}
+
+	switch Magic(binary.LittleEndian.Uint32(magic[:])) {
+	case Magic32, Magic64, MagicFatBE, MagicFatLE:
+		return true, nil
+	default:
 		f.Seek(0, io.SeekStart)
 		if _, err := img4.ParseIm4p(f); err == nil {
 			if strings.Contains(filePath, "kernelcache") {
