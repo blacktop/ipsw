1805c3a59e21aeac55e1da44c14d0fb98a8e005d
blacktop <blacktop@users.noreply.github.com>
2024-01-12 13:08:46 -0700

    Update frida.go

--- cmd/ipsw/cmd/frida/frida.go
+++ cmd/ipsw/cmd/frida/frida.go
@@ -28,7 +28,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const fridaVersion = "16.1.10"
+const fridaVersion = "16.1.11"
 
 func init() {
 	FridaCmd.PersistentFlags().StringP("udid", "u", "", "Device UniqueDeviceID to connect to")
