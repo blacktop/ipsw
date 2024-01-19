cd38545ac0f165366a3bac440ab75c5610025564
blacktop <blacktop@users.noreply.github.com>
2024-01-18 17:26:01 -0700

    fix: `--no-color` not being respected #387

--- cmd/ipsw/cmd/download/download.go
+++ cmd/ipsw/cmd/download/download.go
@@ -210,6 +210,7 @@ var DownloadCmd = &cobra.Command{
 	Args:    cobra.NoArgs,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		viper.BindPFlag("color", cmd.Flags().Lookup("color"))
+		viper.BindPFlag("no-color", cmd.Flags().Lookup("no-color"))
 		viper.BindPFlag("verbose", cmd.Flags().Lookup("verbose"))
 		viper.BindPFlag("diff-tool", cmd.Flags().Lookup("diff-tool"))
 	},
--- cmd/ipsw/cmd/img4/img4.go
+++ cmd/ipsw/cmd/img4/img4.go
@@ -33,6 +33,7 @@ var Img4Cmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		viper.BindPFlag("color", cmd.Flags().Lookup("color"))
+		viper.BindPFlag("no-color", cmd.Flags().Lookup("no-color"))
 		viper.BindPFlag("verbose", cmd.Flags().Lookup("verbose"))
 		viper.BindPFlag("diff-tool", cmd.Flags().Lookup("diff-tool"))
 	},
