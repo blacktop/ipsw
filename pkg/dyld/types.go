25e3beb5d6692d87724d5faa8e133e424b1be492
blacktop <blacktop@users.noreply.github.com>
2024-05-29 13:27:26 -0600

    Update types.go

--- pkg/dyld/types.go
+++ pkg/dyld/types.go
@@ -324,7 +324,7 @@ func (i CacheSlideInfo2) GetPageSize() uint32 {
 	return i.PageSize
 }
 func (i CacheSlideInfo2) SlidePointer(ptr uint64) uint64 {
-	shift := uint64(64 - bits.LeadingZeros64(i.ValueAdd))
+	shift := uint64(bits.Len64(i.ValueAdd))
 	mask := uint64(1<<64-1) >> shift << shift
 	if ptr > i.ValueAdd && (ptr&mask) == 0 {
 		return ptr
