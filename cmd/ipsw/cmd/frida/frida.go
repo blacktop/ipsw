3bd6fb9d9f54d9936adf7d4fed2d50719ad7da36
blacktop <blacktop@users.noreply.github.com>
2024-06-04 16:38:33 -0600

    chore: revert frida due to build errors

--- cmd/ipsw/cmd/frida/frida.go
+++ cmd/ipsw/cmd/frida/frida.go
@@ -28,7 +28,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const fridaVersion = "16.3.3"
+const fridaVersion = "16.2.5"
 
 func init() {
 	FridaCmd.PersistentFlags().StringP("udid", "u", "", "Device UniqueDeviceID to connect to")
