f81033cf5e8337b5b146c013fbac973ceefc3636
blacktop <blacktop@users.noreply.github.com>
2024-07-07 10:05:27 -0600

    add code

--- pkg/usb/backup/device_link.go
+++ pkg/usb/backup/device_link.go
@@ -1,1 +1,12 @@
 package backup
+
+type DeviceLink struct {
+}
+
+func NewDeviceLink(directory string) (*DeviceLink, error) {
+	return nil, nil
+}
+
+func (d *DeviceLink) Close() error {
+	return nil
+}
