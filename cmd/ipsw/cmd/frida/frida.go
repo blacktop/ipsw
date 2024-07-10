81d2ceb3f6782263d811324d6d4a20b80f53a670
blacktop <blacktop@users.noreply.github.com>
2024-07-09 20:37:53 -0600

    chore(deps): bump deps

--- cmd/ipsw/cmd/frida/frida.go
+++ cmd/ipsw/cmd/frida/frida.go
@@ -28,7 +28,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const fridaVersion = "16.3.3"
+const fridaVersion = "16.4.2"
 
 func init() {
 	FridaCmd.PersistentFlags().StringP("udid", "u", "", "Device UniqueDeviceID to connect to")
