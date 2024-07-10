571f91fc4074f66ea5b49d1dac63001146b57602
blacktop <blacktop@users.noreply.github.com>
2024-07-10 14:40:51 -0600

    docs: add API docs for `/aea` route

--- api/server/routes/aea/aea.go
+++ api/server/routes/aea/aea.go
@@ -11,8 +11,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// swagger:response
+type aeaPemResponse []byte
+
 // AddRoutes adds the download routes to the router
 func AddRoutes(rg *gin.RouterGroup, pemDbPath string) {
+	// swagger:route GET /aea/fcs-keys/:key AEA getFcsKeys
+	//
+	// FcsKeys
+	//
+	// Get fsc-keys PEM bytes for a given key.
+	//
+	//     Produces:
+	//     - application/json
+	//     Parameters:
+	//       + name: key
+	//         in: path
+	//         description: fcs-keys.json PEM lookup key
+	//         required: true
+	//         type: string
+	//     Responses:
+	//       200: aeaPemResponse
+	//       500: genericError
 	rg.GET("/aea/fcs-keys/:key", func(c *gin.Context) {
 		key := c.Param("key")
 		if key == "" {
@@ -42,7 +62,7 @@ func AddRoutes(rg *gin.RouterGroup, pemDbPath string) {
 				})
 				return
 			}
-			c.Data(http.StatusOK, "application/octet-stream", pem)
+			c.Data(http.StatusOK, "application/octet-stream", aeaPemResponse(pem))
 		} else {
 			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "key not found"})
 		}
