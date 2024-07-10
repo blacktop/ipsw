522ccbe0ddec9a05bd94759e5cbe50019e15c526
blacktop <blacktop@users.noreply.github.com>
2024-07-10 10:52:47 -0600

    fix: add --delta flag validation to `ipsw dl ota` cmd #514

--- cmd/ipsw/cmd/download/download_ota.go
+++ cmd/ipsw/cmd/download/download_ota.go
@@ -193,6 +193,9 @@ var otaDLCmd = &cobra.Command{
 		if getRSR && len(build) == 0 {
 			return fmt.Errorf("for now you will need to supply a --build number when using --rsr")
 		}
+		if viper.GetBool("download.ota.delta") && len(build) == 0 || len(version) == 0 {
+			return fmt.Errorf("for now you will need to supply a --version AND --build number when using --delta")
+		}
 		if len(build) == 0 {
 			build = "0"
 		}
