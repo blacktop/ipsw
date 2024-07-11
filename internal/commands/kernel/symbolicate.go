cce6f58d4a9324dd865ae7bed260dc1d7264019c
blacktop <blacktop@users.noreply.github.com>
2024-07-11 16:12:49 -0600

    Update symbolicate.go

--- internal/commands/kernel/symbolicate.go
+++ internal/commands/kernel/symbolicate.go
@@ -4,6 +4,7 @@ package kernel
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -11,6 +12,8 @@ import (
 	"github.com/apex/log"
 	"github.com/blacktop/go-macho"
 	"github.com/blacktop/go-macho/types"
+	"github.com/blacktop/ipsw/internal/utils"
+	"github.com/blacktop/ipsw/pkg/disass"
 	"github.com/blacktop/ipsw/pkg/signature"
 )
 
@@ -32,6 +35,72 @@ func ParseSignatures(dir string) (sigs *signature.Symbolicator, err error) {
 	return sigs, nil
 }
 
+func xrefs(m *macho.File, addr uint64) error {
+	xrefs := make(map[uint64]string)
+	symbolMap := make(map[uint64]string)
+
+	utils.Indent(log.Debug, 2)(fmt.Sprintf("Searching for xrefs to: %#x", addr))
+
+	for _, fn := range m.GetFunctions() {
+		data, err := m.GetFunctionData(fn)
+		if err != nil {
+			log.Errorf("failed to get data for function: %v", err)
+			continue
+		}
+
+		engine := disass.NewMachoDisass(m, &symbolMap, &disass.Config{
+			Data:         data,
+			StartAddress: fn.StartAddr,
+		})
+
+		if err := engine.Triage(); err != nil {
+			return fmt.Errorf("first pass triage failed: %v", err)
+		}
+
+		if ok, loc := engine.Contains(addr); ok {
+			if syms, err := m.FindAddressSymbols(fn.StartAddr); err == nil {
+				if len(syms) > 0 {
+					symbolMap[loc] = syms[0].Name
+				}
+				xrefs[loc] = fmt.Sprintf("%s + %d", syms[0].Name, loc-fn.StartAddr)
+			} else {
+				xrefs[loc] = fmt.Sprintf("func_%x + %d", fn.StartAddr, loc-fn.StartAddr)
+			}
+		}
+	}
+
+	for loc, sym := range xrefs {
+		utils.Indent(log.WithFields(log.Fields{
+			"address": fmt.Sprintf("%#09x", loc),
+			"symbol":  sym,
+		}).Info, 2)("XREF")
+	}
+
+	return nil
+}
+
+func symbolicate(m *macho.File, name string, sigs *signature.Symbolicator) error {
+	strs, err := m.GetCStrings()
+	if err != nil {
+		return err
+	}
+	for _, sig := range sigs.Signatures {
+		for addr, s := range strs {
+			if strings.Contains(s, sig.Pattern) {
+				log.WithFields(log.Fields{
+					"pattern": s,
+					"address": fmt.Sprintf("%#09x", addr),
+					"file":    name,
+				}).Info("Found Signature")
+				if err := xrefs(m, addr); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
 func Symbolicate(in string, sigs *signature.Symbolicator) error {
 	m, err := macho.Open(in)
 	if err != nil {
@@ -45,20 +114,14 @@ func Symbolicate(in string, sigs *signature.Symbolicator) error {
 			if err != nil {
 				return err
 			}
-			strs, err := entry.GetCStrings()
-			if err != nil {
+			if err := symbolicate(entry, fs.EntryID, sigs); err != nil {
 				return err
-			}
-			for _, sig := range sigs.Signatures {
-				for _, s := range strs {
-					if strings.Contains(s, sig.Pattern) {
-						log.Infof("Found signature: %s", s)
-					}
-				}
 			}
 		}
 	} else {
-
+		if err := symbolicate(m, filepath.Base(in), sigs); err != nil {
+			return err
+		}
 	}
 
 	return nil
