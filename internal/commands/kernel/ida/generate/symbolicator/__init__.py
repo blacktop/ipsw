83c99af0695ea44ac247e29c585067beb8b09206
blacktop <blacktop@users.noreply.github.com>
2024-07-15 16:29:21 -0600

    chore: add LICENSE to sigs

--- internal/commands/kernel/ida/generate/symbolicator/__init__.py
+++ internal/commands/kernel/ida/generate/symbolicator/__init__.py
@@ -98,8 +98,30 @@ class Symbolicator(object):
     def write_to_pkl(self, filepath: str):
         try:
             with open(filepath, "w", encoding="utf-8") as f:
-                f.write('amends "../pkl/Symbolicator.pkl"\n\n')
-                f.write(f'target = "{self.target}"\n\n')
+                f.write('amends "../pkl/Symbolicator.pkl"\n')
+                f.write('''
+// MIT License
+//
+// Copyright (c) 2024 blacktop
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.''')
+                f.write(f'\n\ntarget = "{self.target}"\n\n')
                 f.write(f"total = {self.total}\n\n")
                 self.version.write(f)
                 f.write("signatures {\n")
