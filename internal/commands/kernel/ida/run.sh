dcc160ed6a42b5f500f0a199b3137e8e3096d9a9
blacktop <blacktop@users.noreply.github.com>
2024-07-15 22:33:21 -0600

    chore: use getenv more in IDApy script

--- internal/commands/kernel/ida/run.sh
+++ internal/commands/kernel/ida/run.sh
@@ -6,12 +6,21 @@ if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
 
-: ${PKL_FILE:="/tmp/kernel_symbols.pkl"}
+# : ${TARGET:=}
+# : ${MAX_VERSION:=}
+# : ${MIN_VERSION:=}
+# : ${PKL_FILE:=}
 
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
     echo 'Usage: run.sh KERNELCACHE_PATH
 
 This script runs the generate.py script in "headless mode" IDA Pro.
+
+[SUPPORTED ENVIRONMENT VARIABLES]
+    TARGET: The target binary. (e.g. com.apple.driver.AppleHIDKeyboard)
+    MAX_VERSION: The maximum version of the target binary.
+    MIN_VERSION: The minimum version of the target binary.
+    PKL_FILE: The path to the pickle file. (e.g. /path/to/sig.pkl)
 
 '
     exit
@@ -22,7 +31,7 @@ main() {
     KERNELCACHE_PATH="$1"
     echo "  🚀 Starting... $KERNELCACHE_PATH"
     # IDA Help: Command line switches - https://www.hex-rays.com/products/ida/support/idadoc/417.shtml
-    PKL_FILE=$PKL_FILE /Applications/IDA\ Pro\ 8.4/ida64.app/Contents/MacOS/idat64 -A -P -S"generate/generate.py" -L/tmp/ida.log $KERNELCACHE_PATH
+    /Applications/IDA\ Pro\ 8.4/ida64.app/Contents/MacOS/idat64 -A -P -S"generate/generate.py" -L/tmp/ida.log $KERNELCACHE_PATH
     echo "  🎉 Done!"
 }
 
