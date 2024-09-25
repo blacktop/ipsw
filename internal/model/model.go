24775d53bb7dda6e07691a319381c48f0e3cef5a
blacktop <blacktop@users.noreply.github.com>
2024-09-25 13:45:18 -0600

    chore: improve storage size of symbol server data (postgres)

--- internal/model/model.go
+++ internal/model/model.go
@@ -22,7 +22,6 @@ type Ipsw struct {
 	Version    string             `json:"version,omitempty"`
 	BuildID    string             `json:"buildid,omitempty"`
 	Devices    []*Device          `gorm:"many2many:ipsw_devices;" json:"devices,omitempty"`
-	Date       time.Time          `json:"date,omitempty"`
 	Kernels    []*Kernelcache     `gorm:"many2many:ipsw_kernels;" json:"kernels,omitempty"`
 	DSCs       []*DyldSharedCache `gorm:"many2many:ipsw_dscs;" json:"dscs,omitempty"`
 	FileSystem []*Macho           `gorm:"many2many:ipsw_files;" json:"file_system,omitempty"`
@@ -34,10 +33,6 @@ type Ipsw struct {
 
 type Device struct {
 	Name string `gorm:"primaryKey" json:"name"`
-
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
 // Kernelcache is the model for a kernelcache.
@@ -62,29 +57,45 @@ type DyldSharedCache struct {
 	Images            []*Macho `gorm:"many2many:dsc_images;" json:"images,omitempty"`
 }
 
+type Path struct {
+	ID   uint   `gorm:"primaryKey"`
+	Path string `gorm:"uniqueIndex" json:"name,omitempty"`
+}
+
 type Macho struct {
 	UUID      string `gorm:"primaryKey" json:"uuid"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-
-	Name      string    `json:"name,omitempty"`
+	PathID    uint
+	Path      Path      `gorm:"foreignKey:PathID"`
 	TextStart uint64    `gorm:"type:bigint" json:"text_start,omitempty"`
 	TextEnd   uint64    `gorm:"type:bigint" json:"text_end,omitempty"`
 	Symbols   []*Symbol `gorm:"many2many:macho_syms;"`
 }
 
+func (m Macho) GetPath() string {
+	return m.Path.Path
+}
+
+type Name struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"uniqueIndex" json:"name,omitempty"`
+}
+
 // swagger:model
 type Symbol struct {
 	// swagger:ignore
-	gorm.Model
-	Symbol string `json:"symbol"`
+	ID     uint `gorm:"primaryKey"`
+	NameID uint
+	Name   Name   `gorm:"foreignKey:NameID"`
 	Start  uint64 `gorm:"type:bigint" json:"start"`
 	End    uint64 `gorm:"type:bigint" json:"end"`
 }
 
+func (s Symbol) GetName() string {
+	return s.Name.Name
+}
+
 func (s Symbol) String() string {
-	return fmt.Sprintf("%#x: %s", s.Start, s.Symbol)
+	return fmt.Sprintf("%#x: %s", s.Start, s.Name.Name)
 }
 
 func (s Symbol) MarshalJSON() ([]byte, error) {
@@ -93,7 +104,7 @@ func (s Symbol) MarshalJSON() ([]byte, error) {
 		Start  uint64 `json:"start"`
 		End    uint64 `json:"end"`
 	}{
-		Symbol: s.Symbol,
+		Symbol: s.Name.Name,
 		Start:  s.Start,
 		End:    s.End,
 	})
