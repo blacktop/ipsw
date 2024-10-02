2510612155066ea03e9e0379a8b52a65c90bb1c7
blacktop <blacktop@users.noreply.github.com>
2024-10-02 12:50:16 -0600

    Create tc.go

---
+++ cmd/ipsw/cmd/fw/tc.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2024 blacktop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package fw
+
+import (
+	"github.com/apex/log"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// NOTE:
+//   Firmware/044-09543-048.dmg.trustcache
+//   Firmware/044-36267-012.dmg.aea.trustcache
+
+func init() {
+	FwCmd.AddCommand(tcCmd)
+
+	tcCmd.Flags().StringP("output", "o", "", "Folder to extract files to")
+	tcCmd.MarkFlagDirname("output")
+	viper.BindPFlag("fw.tc.output", tcCmd.Flags().Lookup("output"))
+}
+
+// tcCmd represents the tc command
+var tcCmd = &cobra.Command{
+	Use:    "tc",
+	Short:  "🚧 Dump TrustCache",
+	Args:   cobra.ExactArgs(1),
+	Hidden: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if viper.GetBool("verbose") {
+			log.SetLevel(log.DebugLevel)
+		}
+
+		// flags
+		// output := viper.GetString("fw.tc.output")
+
+		panic("not implemented")
+
+		return nil
+	},
+}
