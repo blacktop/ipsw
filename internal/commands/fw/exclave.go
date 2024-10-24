e362f83c6c98fb4ab71c53849f5b4f27d7580d25
blacktop <blacktop@users.noreply.github.com>
2024-10-24 00:04:22 -0600

    fix: exclavecore_bundle extraction

--- internal/commands/fw/exclave.go
+++ internal/commands/fw/exclave.go
@@ -32,8 +32,6 @@ func Extract(input, output string) ([]string, error) {
 	}
 	defer f.Close()
 
-	assetIndex := 0
-
 	for idx, bf := range bn.Files {
 		fname := filepath.Join(output, bf.Type, bf.Name)
 		if err := os.MkdirAll(filepath.Dir(fname), 0o750); err != nil {
@@ -46,14 +44,25 @@ func Extract(input, output string) ([]string, error) {
 		}
 		defer of.Close()
 
-		if len(bf.Segments) == 0 { // FIXME: should this be removed?
-			continue
-		}
-
-		entry := bn.Config.TOC[idx].GetEntry()
-
-		switch {
-		case entry == nil: // APP (MachO)
+		if idx == 0 { // SYSTEM/kernel
+			if _, err := f.Seek(int64(bn.Config.Assets[idx].Offset), io.SeekStart); err != nil {
+				return nil, fmt.Errorf("failed to seek to offset %d: %v", bn.Config.Assets[idx].Offset, err)
+			}
+			mHdrData := make([]byte, bn.Config.Assets[idx].Size)
+			if err := binary.Read(f, binary.LittleEndian, &mHdrData); err != nil {
+				return nil, fmt.Errorf("failed to read data from file %s: %v", fname, err)
+			}
+			m, err = macho.NewFile(bytes.NewReader(mHdrData), macho.FileConfig{
+				LoadIncluding: []types.LoadCmd{types.LC_SEGMENT_64},
+			})
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse MachO file: %v", err)
+			}
+			// write MACHOHEADERLC to kernel
+			if _, err := of.Write(mHdrData); err != nil {
+				return nil, fmt.Errorf("failed to write data to file %s: %v", fname, err)
+			}
+		} else {
 			text := bf.Segment("TEXT")
 			if text == nil {
 				text = bf.Segment("HEADER")
@@ -74,72 +83,58 @@ func Extract(input, output string) ([]string, error) {
 			if err != nil {
 				return nil, fmt.Errorf("failed to parse MachO file: %v", err)
 			}
-			defer m.Close()
-		case bf.Type == "SYSTEM": // ASSET (SYSTEM kernel)
-			if _, err := f.Seek(int64(bn.Config.Assets[assetIndex].Offset), io.SeekStart); err != nil {
-				return nil, fmt.Errorf("failed to seek to offset %d: %v", bn.Config.Assets[assetIndex].Offset, err)
-			}
-			mHdrData := make([]byte, bn.Config.Assets[assetIndex].Size) // __MACHOHEADERLC
-			if err := binary.Read(f, binary.LittleEndian, &mHdrData); err != nil {
-				return nil, fmt.Errorf("failed to read data from file %s: %v", fname, err)
-			}
-			m, err = macho.NewFile(bytes.NewReader(mHdrData), macho.FileConfig{
-				LoadIncluding: []types.LoadCmd{types.LC_SEGMENT_64},
-			})
-			if err != nil {
-				return nil, fmt.Errorf("failed to parse MachO file: %v", err)
-			}
-			defer m.Close()
-			// write MACHOHEADERLC to output file
-			if _, err := of.Write(mHdrData); err != nil {
-				return nil, fmt.Errorf("failed to write data to file %s: %v", fname, err)
-			}
-			assetIndex++
-		default: // ASSET (APP non-MachO)
-			fname := filepath.Join(output, bf.Type, string(entry.Name.Bytes))
-			attr, err := os.Create(fname)
-			if err != nil {
-				return nil, fmt.Errorf("failed to create file %s: %v", fname, err)
-			}
-			defer attr.Close()
-			if _, err := f.Seek(int64(bn.Config.Assets[assetIndex].Offset), io.SeekStart); err != nil {
-				return nil, fmt.Errorf("failed to seek to offset %d: %v", bn.Config.Assets[assetIndex].Offset, err)
-			}
-			adata := make([]byte, bn.Config.Assets[assetIndex].Size) // brkr_artifact
-			if err := binary.Read(f, binary.LittleEndian, &adata); err != nil {
-				return nil, fmt.Errorf("failed to read data from file %s: %v", fname, err)
-			}
-			// TODO: parse 'brkr_artifact' which is a map[string]any or a plist essentially
-			// var brkr map[string]any
-			// if _, err := asn1.Unmarshal(adata, &brkr); err != nil {
-			// 	return nil, fmt.Errorf("failed to unmarshal data from file %s: %v", fname, err)
-			// }
-			// os.WriteFile(filepath.Join(output, bf.Type, "data.json"), adata, 0o644)
-			if _, err := attr.Write(adata); err != nil {
-				return nil, fmt.Errorf("failed to write data to file %s: %v", fname, err)
-			}
-			outfiles = append(outfiles, fname)
-			assetIndex++
-			continue
 		}
-
+		// write data to correct offsets
 		for _, sec := range bf.Sections {
 			if _, err := f.Seek(int64(sec.Offset), io.SeekStart); err != nil {
-				return nil, fmt.Errorf("failed to seek to offset %d: %v", sec.Offset, err)
+				return nil, fmt.Errorf("failed to seek to offset %d in file %s: %v", sec.Offset, fname, err)
 			}
 			data := make([]byte, sec.Size)
 			if err := binary.Read(f, binary.LittleEndian, &data); err != nil {
 				return nil, fmt.Errorf("failed to read data from file %s: %v", fname, err)
 			}
 			if s := m.Segment("__" + sec.Name); s == nil { // lookup segment in MachO header
-				return nil, fmt.Errorf("failed to find segment %s", sec.Name)
+				return nil, fmt.Errorf("failed to find segment %s for %s", sec.Name, fname)
 			} else {
 				if _, err := of.WriteAt(data, int64(s.Offset)); err != nil {
 					return nil, fmt.Errorf("failed to write data to file %s: %v", fname, err)
 				}
 			}
 		}
+		m.Close()
+
 		outfiles = append(outfiles, fname)
+
+		/* ASSET file */
+		if entry := bn.Config.TOC[idx].GetEntry(); entry != nil && idx > 0 {
+			for _, asset := range bn.Config.Assets {
+				if entry.Type == asset.Type {
+					aname := fname + "." + string(entry.Name.Bytes)
+					attr, err := os.Create(aname)
+					if err != nil {
+						return nil, fmt.Errorf("failed to create file %s: %v", aname, err)
+					}
+					defer attr.Close()
+					if _, err := f.Seek(int64(asset.Offset), io.SeekStart); err != nil {
+						return nil, fmt.Errorf("failed to seek to offset %d: %v", asset.Offset, err)
+					}
+					adata := make([]byte, asset.Size) // brkr_artifact
+					if err := binary.Read(f, binary.LittleEndian, &adata); err != nil {
+						return nil, fmt.Errorf("failed to read data from file %s: %v", aname, err)
+					}
+					// TODO: parse 'brkr_artifact' which is a map[string]any or a plist essentially
+					// var brkr map[string]any
+					// if _, err := asn1.Unmarshal(adata, &brkr); err != nil {
+					// 	return nil, fmt.Errorf("failed to unmarshal data from file %s: %v", aname, err)
+					// }
+					// os.WriteFile(filepath.Join(output, bf.Type, "data.json"), adata, 0o644)
+					if _, err := attr.Write(adata); err != nil {
+						return nil, fmt.Errorf("failed to write data to file %s: %v", aname, err)
+					}
+					outfiles = append(outfiles, aname)
+				}
+			}
+		}
 	}
 
 	return outfiles, nil
