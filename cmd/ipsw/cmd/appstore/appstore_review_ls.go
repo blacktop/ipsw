b3650a833a49815f239b69e7fb2408a272f57158
kenstir <kenstir@gmail.com>
2025-01-23 18:48:40 -0500

    Remove duplicate BindPFlag calls

--- cmd/ipsw/cmd/appstore/appstore_review_ls.go
+++ cmd/ipsw/cmd/appstore/appstore_review_ls.go
@@ -60,11 +60,6 @@ var ASReviewListCmd = &cobra.Command{
 		}
 		color.NoColor = viper.GetBool("no-color")
 
-		// parent flags
-		viper.BindPFlag("appstore.p8", cmd.Flags().Lookup("p8"))
-		viper.BindPFlag("appstore.iss", cmd.Flags().Lookup("iss"))
-		viper.BindPFlag("appstore.kid", cmd.Flags().Lookup("kid"))
-		viper.BindPFlag("appstore.jwt", cmd.Flags().Lookup("jwt"))
 		// flags
 		id := viper.GetString("appstore.review.ls.id")
 		after := viper.GetString("appstore.review.ls.after")
