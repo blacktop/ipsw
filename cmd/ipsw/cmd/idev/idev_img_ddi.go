b6dee16f2c411c6b2363f8af380496e73e13191f
blacktop <blacktop@users.noreply.github.com>
2025-03-30 12:48:39 -0600

    feat: add new abilities to `ipsw idev img ddi` cmd

--- cmd/ipsw/cmd/idev/idev_img_ddi.go
+++ cmd/ipsw/cmd/idev/idev_img_ddi.go
@@ -24,9 +24,14 @@ THE SOFTWARE.
 package idev
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 
 	"github.com/apex/log"
+	"github.com/blacktop/ipsw/internal/magic"
 	"github.com/blacktop/ipsw/internal/utils"
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
@@ -38,12 +43,17 @@ func init() {
 
 	ddiCmd.Flags().BoolP("info", "i", false, "Show DDI info")
 	ddiCmd.Flags().BoolP("xcode", "x", false, "Update DDI from Xcode")
-	ddiCmd.Flags().StringP("source-dir", "s", "", "Update DDI from source directory")
+	ddiCmd.Flags().StringP("source", "s", "", "Update DDI from source directory")
 	ddiCmd.Flags().BoolP("clean", "c", false, "Clean DDI")
+	ddiCmd.Flags().BoolP("backup", "b", false, "Backup DDI")
+	ddiCmd.Flags().StringP("output", "o", "", "Output directory")
+	ddiCmd.MarkFlagDirname("output")
 	viper.BindPFlag("idev.img.ddi.info", ddiCmd.Flags().Lookup("info"))
 	viper.BindPFlag("idev.img.ddi.xcode", ddiCmd.Flags().Lookup("xcode"))
-	viper.BindPFlag("idev.img.ddi.source-dir", ddiCmd.Flags().Lookup("source-dir"))
+	viper.BindPFlag("idev.img.ddi.source", ddiCmd.Flags().Lookup("source"))
 	viper.BindPFlag("idev.img.ddi.clean", ddiCmd.Flags().Lookup("clean"))
+	viper.BindPFlag("idev.img.ddi.backup", ddiCmd.Flags().Lookup("backup"))
+	viper.BindPFlag("idev.img.ddi.output", ddiCmd.Flags().Lookup("output"))
 }
 
 // ddiCmd represents the ddi command
@@ -59,8 +69,12 @@ var ddiCmd = &cobra.Command{
 		}
 		color.NoColor = viper.GetBool("no-color")
 
-		if !viper.IsSet("idev.img.ddi.info") && !viper.IsSet("idev.img.ddi.xcode") && !viper.IsSet("idev.img.ddi.source-dir") && !viper.IsSet("idev.img.ddi.clean") {
-			return fmt.Errorf("no subcommand provided, must provide one of: --info, --xcode, --source-dir, --clean")
+		if !viper.IsSet("idev.img.ddi.info") &&
+			!viper.IsSet("idev.img.ddi.xcode") &&
+			!viper.IsSet("idev.img.ddi.source") &&
+			!viper.IsSet("idev.img.ddi.clean") &&
+			!viper.IsSet("idev.img.ddi.backup") {
+			return fmt.Errorf("no subcommand provided, must provide one of: --info, --xcode, --source, --clean, --backup")
 		}
 
 		if viper.GetBool("idev.img.ddi.info") {
@@ -68,11 +82,7 @@ var ddiCmd = &cobra.Command{
 			if err != nil {
 				return fmt.Errorf("failed to get preferred DDI: %v", err)
 			}
-			if len(ddi.Result.Platforms.IOS) == 0 &&
-				len(ddi.Result.Platforms.MacOS) == 0 &&
-				len(ddi.Result.Platforms.TvOS) == 0 &&
-				len(ddi.Result.Platforms.WatchOS) == 0 &&
-				len(ddi.Result.Platforms.XrOS) == 0 {
+			if ddi.Empty() {
 				log.Warn("no DDIs found")
 			}
 			for _, platform := range ddi.Result.Platforms.IOS {
@@ -96,12 +106,66 @@ var ddiCmd = &cobra.Command{
 				return fmt.Errorf("failed to update DDIs from Xcode: %v", err)
 			}
 			fmt.Println(out)
-		} else if viper.GetBool("idev.img.ddi.source-dir") {
-			out, err := utils.UpdateDDIs(viper.GetString("idev.img.ddi.source-dir"))
+		} else if viper.IsSet("idev.img.ddi.source") {
+			if isZip, _ := magic.IsZip(viper.GetString("idev.img.ddi.source")); isZip {
+				tmpdir, err := os.MkdirTemp("", "ddi-")
+				if err != nil {
+					return fmt.Errorf("failed to create temporary directory: %v", err)
+				}
+				defer os.RemoveAll(tmpdir)
+
+				log.Infof("Extracting %s to %s", viper.GetString("idev.img.ddi.source"), tmpdir)
+				zipReader, err := zip.OpenReader(viper.GetString("idev.img.ddi.source"))
+				if err != nil {
+					return fmt.Errorf("failed to open zip file: %v", err)
+				}
+				defer zipReader.Close()
+
+				for _, file := range zipReader.File {
+					filePath := filepath.Join(tmpdir, file.Name)
+					// Create directory tree
+					if file.FileInfo().IsDir() {
+						if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
+							return fmt.Errorf("failed to create directory: %v", err)
+						}
+						continue
+					}
+					// Create parent directory if needed
+					if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+						return fmt.Errorf("failed to create parent directory: %v", err)
+					}
+					// Extract file
+					srcFile, err := file.Open()
+					if err != nil {
+						return fmt.Errorf("failed to open file in zip: %v", err)
+					}
+					dstFile, err := os.Create(filePath)
+					if err != nil {
+						srcFile.Close()
+						return fmt.Errorf("failed to create destination file: %v", err)
+					}
+					if _, err := io.Copy(dstFile, srcFile); err != nil {
+						srcFile.Close()
+						dstFile.Close()
+						return fmt.Errorf("failed to copy file contents: %v", err)
+					}
+					srcFile.Close()
+					dstFile.Close()
+				}
+				// Update source to use the extracted path
+				viper.Set("idev.img.ddi.source", tmpdir)
+			}
+			out, err := utils.UpdateDDIs(viper.GetString("idev.img.ddi.source"))
 			if err != nil {
 				return fmt.Errorf("failed to update DDIs from source directory: %v", err)
 			}
 			fmt.Println(out)
+		} else if viper.GetBool("idev.img.ddi.backup") {
+			out, err := utils.BackupDDIs(viper.GetString("idev.img.ddi.output"))
+			if err != nil {
+				return fmt.Errorf("failed to backup DDIs: %v", err)
+			}
+			log.Infof("DDI backup created at: %s", out)
 		} else if viper.GetBool("idev.img.ddi.clean") {
 			out, err := utils.CleanDDIs()
 			if err != nil {
--- internal/utils/devicectl.go
+++ internal/utils/devicectl.go
@@ -1,17 +1,22 @@
 package utils
 
 import (
+	"archive/zip"
 	"encoding/json"
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 
+	"github.com/blacktop/go-plist"
 	"github.com/blacktop/ipsw/pkg/ddi"
 )
 
 const devicectlPath = "/Library/Developer/PrivateFrameworks/CoreDevice.framework/Resources/bin/devicectl"
 
-func PreferredDDI() (*ddi.Info, error) {
+func PreferredDDI() (*ddi.PreferredDDI, error) {
 	if runtime.GOOS != "darwin" {
 		return nil, fmt.Errorf("only supported on macOS")
 	}
@@ -20,7 +25,7 @@ func PreferredDDI() (*ddi.Info, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to get preferred DDI: %v", err)
 	}
-	var info ddi.Info
+	var info ddi.PreferredDDI
 	if err := json.Unmarshal(out, &info); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal preferred DDI: %v", err)
 	}
@@ -49,6 +54,96 @@ func UpdateDDIs(sourceDir string) (string, error) {
 		return "", fmt.Errorf("failed to update DDIs: %v", err)
 	}
 	return string(out), nil
+}
+
+func BackupDDIs(outputDir string) (string, error) {
+	if runtime.GOOS != "darwin" {
+		return "", fmt.Errorf("only supported on macOS")
+	}
+
+	ddiDir := "/Library/Developer/CoreDevice/CandidateDDIs"
+	if _, err := os.Stat(ddiDir); os.IsNotExist(err) {
+		return "", fmt.Errorf("DDI directory does not exist: %s", ddiDir)
+	}
+	versionFile := filepath.Join(ddiDir, "iOS/version.plist")
+	data, err := os.ReadFile(versionFile)
+	if err != nil {
+		return "", fmt.Errorf("failed to read %s: %v", versionFile, err)
+	}
+	var version ddi.Version
+	if _, err := plist.Unmarshal(data, &version); err != nil {
+		return "", fmt.Errorf("failed to unmarshal plist: %v", err)
+	}
+
+	zipFile := filepath.Join(outputDir, fmt.Sprintf("ddis_%s.zip", version.ProductBuildVersion))
+	zf, err := os.Create(zipFile)
+	if err != nil {
+		return "", fmt.Errorf("failed to create zip file: %v", err)
+	}
+	defer zf.Close()
+
+	zw := zip.NewWriter(zf)
+	defer zw.Close()
+
+	if err := filepath.WalkDir(ddiDir, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Get the relative path of the file/directory within hostDDIDir
+		relPath, err := filepath.Rel(ddiDir, path)
+		if err != nil {
+			return fmt.Errorf("failed to get relative path for %s: %w", path, err)
+		}
+
+		// Get file info for header
+		info, err := d.Info()
+		if err != nil {
+			return fmt.Errorf("failed to get file info for %s: %w", path, err)
+		}
+
+		// Create a zip file header
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return fmt.Errorf("failed to create zip header for %s: %w", path, err)
+		}
+
+		header.Name = filepath.ToSlash(relPath) // Use slash separators for zip standard
+
+		if d.IsDir() {
+			header.Name += "/" // Mark it as a directory
+		} else {
+			// Set compression method (optional)
+			header.Method = zip.Deflate
+		}
+
+		// Create the entry in the zip file
+		writer, err := zw.CreateHeader(header)
+		if err != nil {
+			return fmt.Errorf("failed to create zip entry for %s: %w", relPath, err)
+		}
+
+		// If it's a file, copy its contents
+		if !d.IsDir() {
+			file, err := os.Open(path)
+			if err != nil {
+				return fmt.Errorf("failed to open file %s: %w", path, err)
+			}
+			defer file.Close()
+
+			_, err = io.Copy(writer, file)
+			if err != nil {
+				return fmt.Errorf("failed to copy file contents for %s: %w", path, err)
+			}
+		}
+
+		return nil
+	}); err != nil {
+		os.Remove(zipFile) // Clean up incomplete zip file on error
+		return "", fmt.Errorf("failed to walk directory %s: %w", ddiDir, err)
+	}
+
+	return zipFile, nil
 }
 
 func CleanDDIs() (string, error) {
