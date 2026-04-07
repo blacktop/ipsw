24bb3aea35141b8247efdaa9685ca83c761bad39
blacktop <blacktop@users.noreply.github.com>
2026-04-07 10:43:19 -0600

    feat(ota): implement bxdiff batch mode with basesystem_patches support
      The bxdiff command only had single-file mode. Implement batch mode
      that iterates the OTA ZIP, finding BXDIFF patches in both
      AssetData/payloadv2/patches/ and basesystem_patches/. Each patch
      is extracted to a temp file, matched against a base file from the
      --input folder, and applied via bxdiff50.Patch.
    
      Closes #1161

--- cmd/ipsw/cmd/ota/ota_patch_bxdiff.go
+++ cmd/ipsw/cmd/ota/ota_patch_bxdiff.go
@@ -24,56 +24,73 @@ package ota
 import (
 	"archive/zip"
 	"fmt"
+	"io"
+	"os"
 	"path/filepath"
 	"regexp"
 
+	"github.com/apex/log"
 	"github.com/blacktop/ipsw/pkg/info"
 	"github.com/blacktop/ipsw/pkg/ota/bxdiff50"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+// bxdiffPatchRE matches BXDIFF patch files in both patches/ and basesystem_patches/
+var bxdiffPatchRE = regexp.MustCompile(`AssetData/payloadv2/(patches|basesystem_patches)/[^/]+$`)
+
 func init() {
 	otaPatchCmd.AddCommand(otaPatchBxdiffCmd)
 
 	otaPatchBxdiffCmd.Flags().BoolP("single", "s", false, "Patch single file")
+	otaPatchBxdiffCmd.Flags().StringP("input", "i", "", "Input folder containing base files to patch")
 	otaPatchBxdiffCmd.Flags().StringP("output", "o", "", "Output folder")
+	otaPatchBxdiffCmd.MarkFlagDirname("input")
 	otaPatchBxdiffCmd.MarkFlagDirname("output")
 	viper.BindPFlag("ota.patch.bxdiff.single", otaPatchBxdiffCmd.Flags().Lookup("single"))
+	viper.BindPFlag("ota.patch.bxdiff.input", otaPatchBxdiffCmd.Flags().Lookup("input"))
 	viper.BindPFlag("ota.patch.bxdiff.output", otaPatchBxdiffCmd.Flags().Lookup("output"))
 }
 
 // otaPatchBxdiffCmd represents the bxdiff command
 var otaPatchBxdiffCmd = &cobra.Command{
-	Use:           "bxdiff <DELTA> <TARGET>",
+	Use:           "bxdiff <OTA> | -s <DELTA> <TARGET>",
 	Aliases:       []string{"b"},
-	Short:         "Patch BXDIFF50 OTAs",
-	Args:          cobra.ExactArgs(2),
+	Short:         "Patch BXDIFF50 OTAs (patches and basesystem_patches)",
 	SilenceErrors: true,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if viper.GetBool("ota.patch.bxdiff.single") {
+			return cobra.ExactArgs(2)(cmd, args)
+		}
+		return cobra.ExactArgs(1)(cmd, args)
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 
 		// flags
 		single := viper.GetBool("ota.patch.bxdiff.single")
+		input := viper.GetString("ota.patch.bxdiff.input")
 		output := viper.GetString("ota.patch.bxdiff.output")
 
 		if single {
 			return bxdiff50.Patch(args[0], args[1], output)
 		}
 
-		panic("not implemented yet (try --single mode)")
+		if input == "" {
+			return fmt.Errorf("batch mode requires --input (-i) folder containing base files to patch against")
+		}
 
 		patchPath := filepath.Clean(args[0])
 
 		i, err := info.Parse(patchPath)
 		if err != nil {
-			return fmt.Errorf("failed to parse IPSW: %v", err)
+			return fmt.Errorf("failed to parse OTA: %v", err)
 		}
 		infoFolder, err := i.GetFolder()
 		if err != nil {
 			return fmt.Errorf("failed to get OTA folder: %v", err)
 		}
 
-		if len(output) > 0 {
+		if output != "" {
 			output = filepath.Join(output, infoFolder)
 		} else {
 			output = infoFolder
@@ -85,19 +102,81 @@ var otaPatchBxdiffCmd = &cobra.Command{
 		}
 		defer zr.Close()
 
-		var patchFiles []string
+		patched := 0
 		for _, zf := range zr.File {
-			if regexp.MustCompile(`AssetData/payloadv2/patches/.*$`).MatchString(zf.Name) {
-				if !zf.FileInfo().IsDir() {
-					patchFiles = append(patchFiles, zf.Name)
+			if zf.FileInfo().IsDir() {
+				continue
+			}
+			if !bxdiffPatchRE.MatchString(zf.Name) {
+				continue
+			}
+
+			patchName := filepath.Base(zf.Name)
+			// Determine subdirectory from patch path (patches/ vs basesystem_patches/)
+			m := bxdiffPatchRE.FindStringSubmatch(zf.Name)
+			subdir := m[1] // "patches" or "basesystem_patches"
+
+			// Find corresponding base file in input folder
+			basePath := filepath.Join(input, patchName)
+			if _, err := os.Stat(basePath); os.IsNotExist(err) {
+				// Try with subdir prefix
+				basePath = filepath.Join(input, subdir, patchName)
+				if _, err := os.Stat(basePath); os.IsNotExist(err) {
+					log.Warnf("Skipping %s: no base file found at %s", zf.Name, patchName)
+					continue
 				}
 			}
+
+			// Extract patch from ZIP to temp file
+			tmpPatch, err := extractZipEntry(zf)
+			if err != nil {
+				return fmt.Errorf("failed to extract patch %s: %v", zf.Name, err)
+			}
+
+			outDir := filepath.Join(output, subdir)
+			if err := os.MkdirAll(outDir, 0o755); err != nil {
+				os.Remove(tmpPatch)
+				return fmt.Errorf("failed to create output directory: %v", err)
+			}
+
+			log.Infof("Patching %s/%s", subdir, patchName)
+			if err := bxdiff50.Patch(tmpPatch, basePath, outDir); err != nil {
+				os.Remove(tmpPatch)
+				log.Errorf("failed to patch %s: %v", patchName, err)
+				continue
+			}
+			os.Remove(tmpPatch)
+			patched++
 		}
 
-		// FIXME: finish this
+		if patched == 0 {
+			return fmt.Errorf("no BXDIFF patches found or applied in %s", patchPath)
+		}
 
-		_ = patchFiles
-
+		log.Infof("Patched %d files to %s", patched, output)
 		return nil
 	},
+}
+
+// extractZipEntry extracts a ZIP entry to a temporary file and returns its path.
+func extractZipEntry(zf *zip.File) (string, error) {
+	rc, err := zf.Open()
+	if err != nil {
+		return "", err
+	}
+	defer rc.Close()
+
+	tmp, err := os.CreateTemp("", "ipsw-bxdiff-*")
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := io.Copy(tmp, rc); err != nil {
+		tmp.Close()
+		os.Remove(tmp.Name())
+		return "", err
+	}
+
+	tmp.Close()
+	return tmp.Name(), nil
 }
