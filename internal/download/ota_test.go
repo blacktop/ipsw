c8dc87999d09c9e2e4c48b137d1715bc7bb31390
blacktop <blacktop@users.noreply.github.com>
2026-06-09 20:06:45 -0600

    chore(audiences): add version 27 beta audience UUIDs and iOS internal-security audience [skip ci]

--- internal/download/ota_test.go
+++ internal/download/ota_test.go
@@ -2,7 +2,7 @@ package download
 
 import "testing"
 
-func TestAssetAudienceIDsIncludes27DeveloperBetas(t *testing.T) {
+func TestAssetAudienceIDsIncludes27Betas(t *testing.T) {
 	t.Parallel()
 
 	const version = "27"
@@ -15,12 +15,37 @@ func TestAssetAudienceIDsIncludes27DeveloperBetas(t *testing.T) {
 	tests := []struct {
 		platform        string
 		developerBetaID string
+		appleSeedBetaID string
 	}{
-		{platform: "ios", developerBetaID: "a5f921db-50af-448c-8f7e-3f093ca2c954"},
-		{platform: "macos", developerBetaID: "621ba5ab-54b6-4a71-891a-425ac0ce4551"},
-		{platform: "tvos", developerBetaID: "6ca2978e-e976-48b5-9b85-cba646d5dea8"},
-		{platform: "watchos", developerBetaID: "973a069a-8d0c-4247-8239-9493f14ee56e"},
-		{platform: "visionos", developerBetaID: "3796f01d-bf07-45c2-8df6-ad7300055ed9"},
+		{
+			platform:        "ios",
+			developerBetaID: "a5f921db-50af-448c-8f7e-3f093ca2c954",
+			appleSeedBetaID: "c0ef13a7-d2dd-4e85-81c2-6f2b485271c0",
+		},
+		{
+			platform:        "macos",
+			developerBetaID: "621ba5ab-54b6-4a71-891a-425ac0ce4551",
+			appleSeedBetaID: "72407f94-0bee-4e80-a7a3-c246572648dd",
+		},
+		{
+			platform:        "tvos",
+			developerBetaID: "6ca2978e-e976-48b5-9b85-cba646d5dea8",
+			appleSeedBetaID: "077f811f-5ff5-4162-8bed-2820ffc2538f",
+		},
+		{
+			platform:        "watchos",
+			developerBetaID: "973a069a-8d0c-4247-8239-9493f14ee56e",
+			appleSeedBetaID: "19b667c1-6315-436c-ad40-5c27dcd73470",
+		},
+		{
+			platform:        "audioos",
+			appleSeedBetaID: "377d5d3a-03bc-41ae-b1d9-96694d79d5c3",
+		},
+		{
+			platform:        "visionos",
+			developerBetaID: "3796f01d-bf07-45c2-8df6-ad7300055ed9",
+			appleSeedBetaID: "6520bc19-c1ca-4232-b468-24019d13f591",
+		},
 	}
 
 	for _, tt := range tests {
@@ -37,6 +62,10 @@ func TestAssetAudienceIDsIncludes27DeveloperBetas(t *testing.T) {
 
 			if got := audienceVersion.DeveloperBeta; got != tt.developerBetaID {
 				t.Fatalf("%s version %s developer beta audience ID = %q, want %q", tt.platform, version, got, tt.developerBetaID)
+			}
+
+			if got := audienceVersion.AppleSeedBeta; got != tt.appleSeedBetaID {
+				t.Fatalf("%s version %s appleseed (customer) beta audience ID = %q, want %q", tt.platform, version, got, tt.appleSeedBetaID)
 			}
 
 			if got := audiences.LatestVersion(tt.platform); got != version {
